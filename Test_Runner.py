07ba131cc965f3af05d685d323a97773f604e115
evgeniy.l@pandats.com <evPicoLi2012>
2018-05-20 19:57:40 +0300

    develop:The project was edited

--- Test_Runner.py
+++ Test_Runner.py
@@ -3,6 +3,9 @@ from unittest import TestLoader, TestSuite
 import os
 from src.main.python.ui.core.reporter.HTMLTestRunner import HTMLTestRunner
 from src.test.python.ui.avtomation.implemetation.Test_Demo import TestDemo
+from src.test.python.ui.avtomation.implemetation.ca_crm.Test_Add_Demo_Accounts import AddDemoAccounts
+from src.test.python.ui.avtomation.implemetation.ca_crm.Test_Add_New_Live_Accounts import AddNewLiveAccount
+from src.test.python.ui.avtomation.implemetation.ca_crm.Test_Deposit import Deposit
 from src.test.python.ui.avtomation.implemetation.ca_crm.Test_SignUp import SignUp
 
 direct = os.getcwd()
@@ -10,8 +13,10 @@ direct = os.getcwd()
 if __name__ == "__main__":
     loader = TestLoader()
     suite = TestSuite((
-        loader.loadTestsFromTestCase(TestDemo),
-        loader.loadTestsFromTestCase(SignUp)
+        loader.loadTestsFromTestCase(SignUp),
+        loader.loadTestsFromTestCase(AddNewLiveAccount),
+        loader.loadTestsFromTestCase(AddDemoAccounts),
+        loader.loadTestsFromTestCase(Deposit),
     ))
 
     outfile = open(direct + "\Regression.html", "w")
