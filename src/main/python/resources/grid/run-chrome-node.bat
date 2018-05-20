3800a8b2c390150952bfaef2006f3c7f2d072d1d
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-05-20 20:57:43 +0300

    develop:the grid was update

--- src/main/python/resources/grid/run-chrome-node.bat
+++ src/main/python/resources/grid/run-chrome-node.bat
@@ -1,6 +1,9 @@
-set SELENIUM_SERVER=C:/selenium-server-standalone-3.12.0.jar
+set SELENIUM_SERVER=D:\automation-newforexqa\src\main\python\resources\grid\server\selenium-server-standalone-3.12.0.jar
 set HUB_ADDRESS= http://10.0.0.94:4478/grid/register/
-set NODE_CONFIG=D:/automation-newforexqa/src/main/python/resources/grid/configurations/chrome-node-config.json
-set DRIVER=D:/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe
+set NODE_CONFIG=D:\automation-newforexqa\src\main\python\resources\grid\configurations\chrome-node-config.json
+set DRIVER=D:\automation-newforexqa\src\main\python\resources\grid\drivers\chromedriver.exe
 
 java -Dwebdriver.chrome.driver=%DRIVER% -jar %SELENIUM_SERVER% -role node -hub %HUB_ADDRESS% -nodeConfig %NODE_CONFIG%
+
+
+
--- src/main/python/resources/grid/run-hub.bat
+++ src/main/python/resources/grid/run-hub.bat
@@ -1,4 +1,4 @@
-set GRID_SERVER=C:/selenium-server-standalone-3.12.0.jar
-set HUB_CONFIG=D:/automation-newforexqa/src/main/python/resources/grid/configurations/hub-config.json
+set GRID_SERVER=D:\automation-newforexqa\src\main\python\resources\grid\server\selenium-server-standalone-3.12.0.jar
+set HUB_CONFIG=D:\automation-newforexqa\src\main\python\resources\grid\configurations\hub-config.json
 
 java -jar %GRID_SERVER% -role hub -hubConfig %HUB_CONFIG%
