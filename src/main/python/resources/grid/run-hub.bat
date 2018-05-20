2390af4e25314e2a7c107b3f0e5806adf1d8c3cd
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-05-20 20:41:02 +0300

    develop:the grid was update

--- src/main/python/resources/grid/run-hub.bat
+++ src/main/python/resources/grid/run-hub.bat
@@ -1,4 +1,4 @@
-set GRID_SERVER=C:/selenium-server-standalone-3.4.0.jar
+set GRID_SERVER=C:/selenium-server-standalone-3.12.0.jar
 set HUB_CONFIG=D:/automation-newforexqa/src/main/python/resources/grid/configurations/hub-config.json
 
 java -jar %GRID_SERVER% -role hub -hubConfig %HUB_CONFIG%
