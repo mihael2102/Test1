6d925a3c7aa7c7666e8af372f61c370682a6c114
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-06-08 11:03:46 +0300

    develop: refactoring of code

--- Test_Runner.py
+++ Test_Runner.py
@@ -1,12 +1,10 @@
 import unittest
 from unittest import TestLoader, TestSuite
 from src.test.python.ui.automation.implementation.Test_Demo import TestDemo
-from src.test.python.ui.automation.implementation.ca.Test_Add_New_Live_Accounts import AddNewLiveAccountTestCA
 
 if __name__ == "__main__":
     loader = TestLoader()
     suite = TestSuite((
-        loader.loadTestsFromTestCase(AddNewLiveAccountTestCA),
         loader.loadTestsFromTestCase(TestDemo)
     ))
 
