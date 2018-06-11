d85dea08e0187bee45edfcdd972725f14b3db083
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-06-11 14:40:39 +0300

    develop: refactoring of code

--- Test_Demo.py
+++ Test_Demo.py
@@ -1,4 +1,5 @@
 from src.main.python.ui.brand.model.pages.home.BrandHomePage import BrandHomePage
+from src.main.python.ui.crm.model.pages.login.CRMLoginPage import CRMLoginPage
 from src.test.python.ui.automation.BaseTest import *
 from src.test.python.utils.TestDataConstants import TestDataConstants
 
@@ -10,3 +11,10 @@ class TestDemo(BaseTest):
             .set_fields(Config.data.get_data_first_client(TestDataConstants.E_MAIL),
                         Config.data.get_data_first_client(TestDataConstants.PASSWORD)) \
             .click_login_button()
+
+        CRMLoginPage() \
+            .open_second_tab_page(Config.url_crm) \
+            .crm_login(Config.data.get_data_first_client(TestDataConstants.USER_NAME),
+                       Config.data.get_data_first_client(TestDataConstants.CRM_PASSWORD))
+
+
