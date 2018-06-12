cfc17aebef26e26d44b12ba6784adcf115bcd59f
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-06-12 12:48:17 +0300

    develop: refactoring of code

--- Test_Runner.py
+++ Test_Runner.py
@@ -1,21 +1,9 @@
-import pytest
 from src.test.python.ui.automation.implementation.ca.Test_Add_Demo_Accounts import AddDemoAccountsTestCA
 from src.test.python.ui.automation.implementation.ca.Test_Add_New_Live_Accounts import AddNewLiveAccountTestCA
-from src.test.python.ui.automation.implementation.ca.Test_Change_Password import ChangePasswordTestCA
-from src.test.python.ui.automation.implementation.ca.Test_Download_Documents import DownloadDocumentsTestCA
 from src.test.python.ui.automation.implementation.ca.Test_Make_Deposit import DepositTestCa
-from src.test.python.ui.automation.implementation.ca.Test_Make_Transfer_Funds import TransferFundsTestCA
-from src.test.python.ui.automation.implementation.ca.Test_Make_Withdraw import WithdrawTestCA
 from src.test.python.ui.automation.implementation.ca.Test_Personal_Details_Update import PersonalDetailsUpdateTestCA
 from src.test.python.ui.automation.implementation.ca.Test_SignUp import SignUpTest
 from src.test.python.ui.automation.implementation.ca.Test_Support_Ticket import CreateSupportTicketTestCa
-from src.test.python.ui.automation.implementation.crm.Test_Add_Interaction import AddInteraction
-from src.test.python.ui.automation.implementation.crm.Test_Change_Password import ChangePasswordTestCRM
-from src.test.python.ui.automation.implementation.crm.Test_Check_Password import CheckPasswordTestCRM
-from src.test.python.ui.automation.implementation.crm.Test_Make_Credit_In import CreditInTestCRM
-from src.test.python.ui.automation.implementation.crm.Test_Make_Credit_Out import CreditOutTestCRM
-from src.test.python.ui.automation.implementation.crm.Test_Make_Deposit import DepositTestCRM
-from src.test.python.ui.automation.implementation.crm.Test_Transfer_Between_Ta import TransferBetweenTa
 
 
 class TestRunner(object):
@@ -32,41 +20,43 @@ class TestRunner(object):
     def run_create_ticket(self):
         CreateSupportTicketTestCa()
 
-    def run_deposit_ca(self):
-        DepositTestCa()
-
-    def run_withdraw(self):
-        WithdrawTestCA()
-
-    def run_transfer_funds(self):
-        TransferFundsTestCA()
-
-    def run_upload_documents(self):
-        DownloadDocumentsTestCA()
-
-    def run_change_password(self):
-        ChangePasswordTestCA()
-
     def run_client_update(self):
         PersonalDetailsUpdateTestCA()
 
-    def run_deposit_crm(self):
-        DepositTestCRM()
+    def run_deposit_ca(self):
+        DepositTestCa()
 
-    def run_credit_in_crm(self):
-        CreditInTestCRM()
+    # def run_withdraw(self):
+    #     WithdrawTestCA()
+    #
+    # def run_transfer_funds(self):
+    #     TransferFundsTestCA()
+    #
+    # def run_upload_documents(self):
+    #     DownloadDocumentsTestCA()
+    #
+    # def run_change_password(self):
+    #     ChangePasswordTestCA()
+    #
 
-    def run_credit_out_crm(self):
-        CreditOutTestCRM()
-
-    def run_transfer_between_ta_crm(self):
-        TransferBetweenTa()
-
-    def run_check_password_crm(self):
-        CheckPasswordTestCRM()
-
-    def run_change_password_crm(self):
-        ChangePasswordTestCRM()
-
-    def run_add_interaction_crm(self):
-        AddInteraction()
+    #
+    # def run_deposit_crm(self):
+    #     DepositTestCRM()
+    #
+    # def run_credit_in_crm(self):
+    #     CreditInTestCRM()
+    #
+    # def run_credit_out_crm(self):
+    #     CreditOutTestCRM()
+    #
+    # def run_transfer_between_ta_crm(self):
+    #     TransferBetweenTa()
+    #
+    # def run_check_password_crm(self):
+    #     CheckPasswordTestCRM()
+    #
+    # def run_change_password_crm(self):
+    #     ChangePasswordTestCRM()
+    #
+    # def run_add_interaction_crm(self):
+    #     AddInteraction()
--- src/test/python/ui/automation/implementation/ca/Test_Add_Demo_Accounts.py
+++ src/test/python/ui/automation/implementation/ca/Test_Add_Demo_Accounts.py
@@ -54,124 +54,124 @@ class AddDemoAccountsTestCA(BaseTest):
         assert usd_currency_ca == usd_currency_crm
         assert balance_ca == balance_crm
 
-    def test_check_add_demo_account_eur_currency(self):
-        BrandHomePage().open_first_tab_page(Config.url_client_area).login() \
-            .set_fields(Config.data.get_data_first_client(TestDataConstants.E_MAIL),
-                        Config.data.get_data_first_client(TestDataConstants.PASSWORD)) \
-            .click_login_button() \
-            .open_drop_down_menu() \
-            .select_module(CaConstants.MANAGE_ACCOUNTS)
-
-        brand_accounts_module = CaManageAccounts() \
-            .open_demo_button() \
-            .open_new_account_button() \
-            .select_account_currency(Config.data.get_data_first_client(TestDataConstants.ACCOUNT_CURRENCY_EUR)) \
-            .set_initial_deposit(Config.data.get_data_first_client(TestDataConstants.INITIAL_DEPOSIT_SECOND)) \
-            .create_account_button()
-
-        account_id_ca = brand_accounts_module.get_account_id_text()
-        eur_currency_ca = brand_accounts_module.get_account_currency_text()
-        balance_ca = brand_accounts_module.get_deposit_balance()
-
-        crm_client_profile = CRMLoginPage() \
-            .open_second_tab_page(Config.url_crm) \
-            .crm_login(Config.data.get_data_first_client(TestDataConstants.USER_NAME),
-                       Config.data.get_data_first_client(TestDataConstants.CRM_PASSWORD)) \
-            .select_filter(Config.data.get_data_first_client(TestDataConstants.FILTER)) \
-            .find_client_by_email(Config.data.get_data_first_client(TestDataConstants.E_MAIL))
-
-        account_id_crm = crm_client_profile \
-            .perform_scroll_down() \
-            .open_trading_accounts_tab() \
-            .open_client_account(account_id_ca) \
-            .get_account_text()
-
-        trading_accounts_info = CRMTradingAccountsInformationPage()
-
-        eur_currency_crm = trading_accounts_info.get_currency_text()
-        balance_crm = trading_accounts_info.get_balance_text()
-
-        assert account_id_ca == account_id_crm
-        assert eur_currency_ca == eur_currency_crm
-        assert balance_ca == balance_crm
-
-    def test_check_add_demo_account_gbr_currency(self):
-        BrandHomePage().open_first_tab_page(Config.url_client_area).login() \
-            .set_fields(Config.data.get_data_first_client(TestDataConstants.E_MAIL),
-                        Config.data.get_data_first_client(TestDataConstants.PASSWORD)) \
-            .click_login_button() \
-            .open_drop_down_menu() \
-            .select_module(CaConstants.MANAGE_ACCOUNTS)
-
-        brand_accounts_module = CaManageAccounts() \
-            .open_demo_button() \
-            .open_new_account_button() \
-            .select_account_currency(Config.data.get_data_first_client(TestDataConstants.ACCOUNT_CURRENCY_GBR)) \
-            .set_initial_deposit(Config.data.get_data_first_client(TestDataConstants.INITIAL_DEPOSIT_THIRD)) \
-            .create_account_button()
-
-        account_id_ca = brand_accounts_module.get_account_id_text()
-        gbr_currency_ca = brand_accounts_module.get_account_currency_text()
-        balance_ca = brand_accounts_module.get_deposit_balance()
-
-        crm_client_profile = CRMLoginPage() \
-            .open_second_tab_page(Config.url_crm) \
-            .crm_login(Config.data.get_data_first_client(TestDataConstants.USER_NAME),
-                       Config.data.get_data_first_client(TestDataConstants.CRM_PASSWORD)) \
-            .select_filter(Config.data.get_data_first_client(TestDataConstants.FILTER)) \
-            .find_client_by_email(Config.data.get_data_first_client(TestDataConstants.E_MAIL))
-
-        account_id_crm = crm_client_profile \
-            .perform_scroll_down() \
-            .open_trading_accounts_tab() \
-            .open_client_account(account_id_ca) \
-            .get_account_text()
-
-        trading_accounts_info = CRMTradingAccountsInformationPage()
-
-        gbr_currency_crm = trading_accounts_info.get_currency_text()
-        balance_crm = trading_accounts_info.get_balance_text()
-
-        assert account_id_ca == account_id_crm
-        assert gbr_currency_ca == gbr_currency_crm
-        assert balance_ca == balance_crm
-
-    def test_check_add_demo_account_cad_currency(self):
-        BrandHomePage().open_first_tab_page(Config.url_client_area).login() \
-            .set_fields(Config.data.get_data_first_client(TestDataConstants.E_MAIL),
-                        Config.data.get_data_first_client(TestDataConstants.PASSWORD)) \
-            .click_login_button() \
-            .open_drop_down_menu() \
-            .select_module(CaConstants.MANAGE_ACCOUNTS)
-
-        brand_accounts_module = CaManageAccounts() \
-            .open_demo_button() \
-            .open_new_account_button() \
-            .select_account_currency(Config.data.get_data_first_client(TestDataConstants.ACCOUNT_CURRENCY_CAD)) \
-            .set_initial_deposit(Config.data.get_data_first_client(TestDataConstants.INITIAL_DEPOSIT_FOURTH)) \
-            .create_account_button()
-
-        account_id_ca = brand_accounts_module.get_account_id_text()
-        cad_currency_ca = brand_accounts_module.get_account_currency_text()
-        balance_ca = brand_accounts_module.get_deposit_balance()
-
-        crm_client_profile = CRMLoginPage() \
-            .open_second_tab_page(Config.url_crm) \
-            .crm_login(Config.data.get_data_first_client(TestDataConstants.USER_NAME),
-                       Config.data.get_data_first_client(TestDataConstants.CRM_PASSWORD)) \
-            .select_filter(Config.data.get_data_first_client(TestDataConstants.FILTER)) \
-            .find_client_by_email(Config.data.get_data_first_client(TestDataConstants.E_MAIL))
-
-        account_id_crm = crm_client_profile \
-            .perform_scroll_down() \
-            .open_trading_accounts_tab() \
-            .open_client_account(account_id_ca) \
-            .get_account_text()
-
-        trading_accounts_info = CRMTradingAccountsInformationPage()
-        cad_currency_crm = trading_accounts_info.get_currency_text()
-        balance_crm = trading_accounts_info.get_balance_text()
-
-        assert account_id_ca == account_id_crm
-        assert cad_currency_ca == cad_currency_crm
-        assert balance_ca == balance_crm
+    # def test_check_add_demo_account_eur_currency(self):
+    #     BrandHomePage().open_first_tab_page(Config.url_client_area).login() \
+    #         .set_fields(Config.data.get_data_first_client(TestDataConstants.E_MAIL),
+    #                     Config.data.get_data_first_client(TestDataConstants.PASSWORD)) \
+    #         .click_login_button() \
+    #         .open_drop_down_menu() \
+    #         .select_module(CaConstants.MANAGE_ACCOUNTS)
+    #
+    #     brand_accounts_module = CaManageAccounts() \
+    #         .open_demo_button() \
+    #         .open_new_account_button() \
+    #         .select_account_currency(Config.data.get_data_first_client(TestDataConstants.ACCOUNT_CURRENCY_EUR)) \
+    #         .set_initial_deposit(Config.data.get_data_first_client(TestDataConstants.INITIAL_DEPOSIT_SECOND)) \
+    #         .create_account_button()
+    #
+    #     account_id_ca = brand_accounts_module.get_account_id_text()
+    #     eur_currency_ca = brand_accounts_module.get_account_currency_text()
+    #     balance_ca = brand_accounts_module.get_deposit_balance()
+    #
+    #     crm_client_profile = CRMLoginPage() \
+    #         .open_second_tab_page(Config.url_crm) \
+    #         .crm_login(Config.data.get_data_first_client(TestDataConstants.USER_NAME),
+    #                    Config.data.get_data_first_client(TestDataConstants.CRM_PASSWORD)) \
+    #         .select_filter(Config.data.get_data_first_client(TestDataConstants.FILTER)) \
+    #         .find_client_by_email(Config.data.get_data_first_client(TestDataConstants.E_MAIL))
+    #
+    #     account_id_crm = crm_client_profile \
+    #         .perform_scroll_down() \
+    #         .open_trading_accounts_tab() \
+    #         .open_client_account(account_id_ca) \
+    #         .get_account_text()
+    #
+    #     trading_accounts_info = CRMTradingAccountsInformationPage()
+    #
+    #     eur_currency_crm = trading_accounts_info.get_currency_text()
+    #     balance_crm = trading_accounts_info.get_balance_text()
+    #
+    #     assert account_id_ca == account_id_crm
+    #     assert eur_currency_ca == eur_currency_crm
+    #     assert balance_ca == balance_crm
+    #
+    # def test_check_add_demo_account_gbr_currency(self):
+    #     BrandHomePage().open_first_tab_page(Config.url_client_area).login() \
+    #         .set_fields(Config.data.get_data_first_client(TestDataConstants.E_MAIL),
+    #                     Config.data.get_data_first_client(TestDataConstants.PASSWORD)) \
+    #         .click_login_button() \
+    #         .open_drop_down_menu() \
+    #         .select_module(CaConstants.MANAGE_ACCOUNTS)
+    #
+    #     brand_accounts_module = CaManageAccounts() \
+    #         .open_demo_button() \
+    #         .open_new_account_button() \
+    #         .select_account_currency(Config.data.get_data_first_client(TestDataConstants.ACCOUNT_CURRENCY_GBR)) \
+    #         .set_initial_deposit(Config.data.get_data_first_client(TestDataConstants.INITIAL_DEPOSIT_THIRD)) \
+    #         .create_account_button()
+    #
+    #     account_id_ca = brand_accounts_module.get_account_id_text()
+    #     gbr_currency_ca = brand_accounts_module.get_account_currency_text()
+    #     balance_ca = brand_accounts_module.get_deposit_balance()
+    #
+    #     crm_client_profile = CRMLoginPage() \
+    #         .open_second_tab_page(Config.url_crm) \
+    #         .crm_login(Config.data.get_data_first_client(TestDataConstants.USER_NAME),
+    #                    Config.data.get_data_first_client(TestDataConstants.CRM_PASSWORD)) \
+    #         .select_filter(Config.data.get_data_first_client(TestDataConstants.FILTER)) \
+    #         .find_client_by_email(Config.data.get_data_first_client(TestDataConstants.E_MAIL))
+    #
+    #     account_id_crm = crm_client_profile \
+    #         .perform_scroll_down() \
+    #         .open_trading_accounts_tab() \
+    #         .open_client_account(account_id_ca) \
+    #         .get_account_text()
+    #
+    #     trading_accounts_info = CRMTradingAccountsInformationPage()
+    #
+    #     gbr_currency_crm = trading_accounts_info.get_currency_text()
+    #     balance_crm = trading_accounts_info.get_balance_text()
+    #
+    #     assert account_id_ca == account_id_crm
+    #     assert gbr_currency_ca == gbr_currency_crm
+    #     assert balance_ca == balance_crm
+    #
+    # def test_check_add_demo_account_cad_currency(self):
+    #     BrandHomePage().open_first_tab_page(Config.url_client_area).login() \
+    #         .set_fields(Config.data.get_data_first_client(TestDataConstants.E_MAIL),
+    #                     Config.data.get_data_first_client(TestDataConstants.PASSWORD)) \
+    #         .click_login_button() \
+    #         .open_drop_down_menu() \
+    #         .select_module(CaConstants.MANAGE_ACCOUNTS)
+    #
+    #     brand_accounts_module = CaManageAccounts() \
+    #         .open_demo_button() \
+    #         .open_new_account_button() \
+    #         .select_account_currency(Config.data.get_data_first_client(TestDataConstants.ACCOUNT_CURRENCY_CAD)) \
+    #         .set_initial_deposit(Config.data.get_data_first_client(TestDataConstants.INITIAL_DEPOSIT_FOURTH)) \
+    #         .create_account_button()
+    #
+    #     account_id_ca = brand_accounts_module.get_account_id_text()
+    #     cad_currency_ca = brand_accounts_module.get_account_currency_text()
+    #     balance_ca = brand_accounts_module.get_deposit_balance()
+    #
+    #     crm_client_profile = CRMLoginPage() \
+    #         .open_second_tab_page(Config.url_crm) \
+    #         .crm_login(Config.data.get_data_first_client(TestDataConstants.USER_NAME),
+    #                    Config.data.get_data_first_client(TestDataConstants.CRM_PASSWORD)) \
+    #         .select_filter(Config.data.get_data_first_client(TestDataConstants.FILTER)) \
+    #         .find_client_by_email(Config.data.get_data_first_client(TestDataConstants.E_MAIL))
+    #
+    #     account_id_crm = crm_client_profile \
+    #         .perform_scroll_down() \
+    #         .open_trading_accounts_tab() \
+    #         .open_client_account(account_id_ca) \
+    #         .get_account_text()
+    #
+    #     trading_accounts_info = CRMTradingAccountsInformationPage()
+    #     cad_currency_crm = trading_accounts_info.get_currency_text()
+    #     balance_crm = trading_accounts_info.get_balance_text()
+    #
+    #     assert account_id_ca == account_id_crm
+    #     assert cad_currency_ca == cad_currency_crm
+    #     assert balance_ca == balance_crm
