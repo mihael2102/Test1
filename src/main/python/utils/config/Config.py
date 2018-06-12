2fd4f49680550f47a27c30e916e4548edff4141f
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-06-12 12:54:16 +0300

    develop: refactoring of code

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,7 +1,7 @@
 browser = None
 timeout = 4
 data = None
-url_new_forex = "https://newforexqa.pandats.com/"
+url_client_area = "https://newforexqa.pandats.com/"
 url_crm = "https://newforexqa.ptscrm.com"
 window_after = None
 window_before = None
