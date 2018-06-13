e0ccdcaa188942ad4b9f9028368f440e531eac01
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-06-13 12:27:52 +0300

    develop: refactoring of code

---
+++ Conftest.py
@@ -0,0 +1,6 @@
+
+def pytest_addoption(parser):
+    parser.addoption("--driver", action="store", default="chrome", help="Type in browser type")
+    parser.addoption("--url", action="store", default="https://qa.moodle.net/", help="url")
+    parser.addoption("--username", action="store", default="manager", help="username")
+    parser.addoption("--password", action="store", default="test", help="password")
