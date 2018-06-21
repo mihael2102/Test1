f40209f913556abf9f598bccde64183af81aba87
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-06-21 16:04:58 +0300

    develop: refactoring of code

---
+++ src/main/python/ui/crm/model/modules/client_modules/mass_sms/CRMSendSMSModule.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from src.main.python.ui.crm.model.crm_base_page.CRMBasePage import CRMBasePage
+from src.main.python.utils.logs.Loging import Logging
+
+
+class CRMSendSMSModule(CRMBasePage):
+
+    def __init__(self):
+        super().__init__()
+
+    def perform_send_sms(self, message):
+        self.enter_message(message)
+        return CRMSendSMSModule()
+
+    def enter_message(self, message):
+        message_field = super().wait_element_to_be_clickable("//textarea[@id='sms_message']")
+        message_field.clear()
+        message_field.send_keys(message)
+        Logging().reportDebugStep(self, "The message was entered " + message)
+        return CRMSendSMSModule()
+
+    def click_send_button(self):
+        send_button = self.driver.find_element(By.XPATH, "//button[contains(text(),'Send')]")
+        send_button.click()
+        Logging().reportDebugStep(self, "The send button was clicked")
+        return CRMSendSMSModule()
+
+    def click_ok(self):
+        super().click_ok()
+        Logging().reportDebugStep(self, "Message sent successfully")
+        return CRMSendSMSModule()
