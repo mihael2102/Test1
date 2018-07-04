aa7689b1ca85984446d861cf3037052100ce0d51
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-07-04 18:08:02 +0300

    develop: refactoring of code

--- src/test/python/ui/automation/implementation/crm/tests_clients_module/Test_Send_SMS_Clients_Module.py
+++ src/test/python/ui/automation/implementation/crm/tests_clients_module/Test_Send_SMS_Clients_Module.py
@@ -15,7 +15,7 @@ class SendSMSClientsModule(BaseTest):
             .perform_searching(Config.data.get_data_client(CRMConstants.CLIENT_STATUS),
                                Config.data.get_data_client(CRMConstants.SHORT_E_MAIL),
                                Config.data.get_data_client(CRMConstants.SHORT_CLIENT_NAME),
-                               Config.data.get_data_client(TestDataConstants.FIRST_COUNTRY),
+                               Config.data.get_data_client(TestDataConstants.SECOND_COUNTRY),
                                Config.data.get_data_client(CRMConstants.SHORT_FIRST_NAME),
                                Config.data.get_data_client(CRMConstants.SHORT_LAST_NAME),
                                Config.data.get_data_client(TestDataConstants.CITY),
