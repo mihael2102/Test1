451f5496685e6a03e06a96f562895963be275f9b
dmytro.k <dmytro.k@pandats.com>
2018-07-09 19:18:13 +0300

    1001: refactoring of script

--- Run_Change_Env.py
+++ Run_Change_Env.py
@@ -11,12 +11,12 @@ with open(path_to_file, "a") as f:
     for i, line in enumerate(fileinput.input(path_to_file)):
         if line.startswith("url_client_area"):
             tmp_list = line.split("\"")
-            tmp_list[1] = new_url_crm
+            tmp_list[1] = url_client_area
             new_line = "\"".join(tmp_list)
             collector.append(new_line)
         elif line.startswith("url_crm"):
             tmp_list = line.split("\"")
-            tmp_list[1] = url_client_area
+            tmp_list[1] = new_url_crm
             new_line = "\"".join(tmp_list)
             collector.append(new_line)
         else:
