6fb8655507851750e2cfc1272827ee9adcccc1fa
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-07-10 13:07:15 +0300

    1000: Refactoring of code

--- src/main/python/ui/crm/model/constants/CRMConstants.py
+++ src/main/python/ui/crm/model/constants/CRMConstants.py
@@ -24,7 +24,7 @@ class CRMConstants(object):
     TENTH_COLUMN = "tenth_column"
     TENTH_COLUMN_OTHER_TYPE = "City"
     ELEVENTH_COLUMN = "eleventh_column"
-    FILTER_NAME = "test_filter2"
+    FILTER_NAME = "test_filter"
     SECOND_FILTER_NAME = "TestFilter" + str(random.randrange(1, 9999))
     CLIENT_STATUS = "client_status_1"
     BRAND_NEW_FOREX = "brand_one"
