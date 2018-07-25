cf45d506d7fae008b096241a056ec2459d8d40ae
dmytro.k <dmytro.k@pandats.com>
2018-07-25 18:46:19 +0300

    1001: refactoring of "Create affiliate" test; developing "Edit affiliate" test

--- src/main/python/ui/crm/model/pages/affiliates/AffiliateListViewPage.py
+++ src/main/python/ui/crm/model/pages/affiliates/AffiliateListViewPage.py
@@ -2,10 +2,12 @@ from time import sleep
 
 from src.main.python.ui.crm.model.pages.crm_base_page.CRMBasePage import CRMBasePage
 from src.main.python.ui.crm.model.modules.affiliates.CreateAffiliateModule import CreateAffiliateModule
+from src.main.python.ui.crm.model.modules.affiliates.EditAffiliateModule import EditAffiliateModule
 from src.main.python.ui.crm.model.pages.affiliates.AffiliateDetailsViewPage import AffiliateDetailsViewPage
 from src.main.python.utils.logs.Loging import Logging
 from selenium.webdriver.common.by import By
 from src.main.python.ui.crm.model.constants.AffiliateModuleConstants import AffiliateModuleConstants
+from src.main.python.utils.config import Config
 
 
 class AffiliateListViewPage(CRMBasePage):
@@ -23,6 +25,12 @@ class AffiliateListViewPage(CRMBasePage):
         Logging().reportDebugStep(self, "Open 'Add new affiliate' popup")
         return CreateAffiliateModule()
 
+    def open_edit_affiliate_popup(self):
+        edit_affiliate_button = super().wait_element_to_be_clickable("//tr[3]/td[10]/div/span")
+        edit_affiliate_button.click()
+        Logging().reportDebugStep(self, "Open 'Edit affiliate' popup")
+        return EditAffiliateModule()
+
     def open_affiliate_details_page(self, existed_partner_name):
         sleep(1)
         partner_name_from_list = self.driver.find_element(By.XPATH, "//grid-cell/div/link-spa/a[contains(., '%s')]" % existed_partner_name)
@@ -38,7 +46,7 @@ class AffiliateListViewPage(CRMBasePage):
         sleep(1)
         partner_name_input.clear()
         partner_name_input.send_keys(partner_name_text)
-
+        sleep(1)
         Logging().reportDebugStep(self, "Searching by partner name: " + partner_name_text)
         return  AffiliateListViewPage()
 
@@ -65,6 +73,58 @@ class AffiliateListViewPage(CRMBasePage):
     def get_brand_name(self):
         brand_name_text = self.driver.find_element(By.XPATH, "//td[9]/grid-cell/div/span[2]").text
         return brand_name_text
+
+    def check_blocked_countries(self):
+        list_of_counties = []
+        list_of_counties.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                   AffiliateModuleConstants.FIRST_COUNTRY))
+        list_of_counties.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                   AffiliateModuleConstants.SECOND_COUNTRY))
+        list_of_counties.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                   AffiliateModuleConstants.THIRD_COUNTRY))
+        list_of_counties.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                   AffiliateModuleConstants.FOURTH_COUNTRY))
+        list_of_counties.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                   AffiliateModuleConstants.FIFTH_COUNTRY))
+
+        """ Get country from affiliate list view page and compare them with initial values from JSON"""
+        for i in list_of_counties:
+            if i in self.get_blocked_countries():
+                continue
+            else:
+                self.fail("Selected country '%s' is not in the list" % i)
+        return True
+
+    def check_allowed_methods(self):
+        list_of_allowed_methods = []
+        list_of_allowed_methods.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                          AffiliateModuleConstants.FIRST_ALLOWED_METHOD_NAME))
+        list_of_allowed_methods.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                          AffiliateModuleConstants.SECOND_ALLOWED_METHOD_NAME))
+        list_of_allowed_methods.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                          AffiliateModuleConstants.THIRD_ALLOWED_METHOD_NAME))
+        list_of_allowed_methods.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                          AffiliateModuleConstants.FOURTH_ALLOWED_METHOD_NAME))
+        list_of_allowed_methods.append(Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                          AffiliateModuleConstants.FIFTH_ALLOWED_METHOD_NAME))
+
+        """ Get country from affiliate list view page and compare them with initial values from JSON"""
+        for i in list_of_allowed_methods:
+            if i in self.get_allowed_methods():
+                continue
+            else:
+                self.fail("Selected allowed method '%s' is not in the list. We expect one of following methods %s" % (
+                i, self.get_allowed_methods()))
+        return True
+
+    def edit_affiliate(self, partner_name, brand, allowed_ip, is_enabled, allowed_methods_1,
+                                 allowed_methods_2, allowed_methods_3, allowed_methods_4, allowed_methods_5,
+                                 blocked_countries_1, blocked_countries_2, blocked_countries_3, blocked_countries_4,
+                                 blocked_countries_5):
+        return self.open_edit_affiliate_popup().
+
+
+
 
 
 
--- src/test/python/ui/automation/utils/preconditions/affiliates/Affiliates_Precondition.py
+++ src/test/python/ui/automation/utils/preconditions/affiliates/Affiliates_Precondition.py
@@ -1,16 +1,49 @@
-from src.main.python.ui.crm.model.constants.LeadsModuleConstants import LeadsModuleConstants
+from src.main.python.ui.crm.model.constants.AffiliateModuleConstants import AffiliateModuleConstants
 from src.main.python.ui.crm.model.pages.home_page.CRMHomePage import CRMHomePage
-from src.main.python.ui.crm.model.pages.leads.LeadDetailViewInfo import LeadDetailViewInfo
 from src.main.python.ui.crm.model.pages.login.CRMLoginPage import CRMLoginPage
 from src.main.python.utils.config import Config
-from src.test.python.utils.TestDataConstants import TestDataConstants
+from src.main.python.ui.crm.model.constants.TestDataConstants import TestDataConstants
 
 
 class AffiliatesPrecondition(object):
 
-    def __init__(self) -> None:
-        super().__init__()
-
     def create_affiliate(self):
-        pass
-        # TBD
+        """ Login to CRM """
+        CRMLoginPage().open_first_tab_page(Config.url_crm) \
+            .crm_login(Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.USER_NAME),
+                       Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.CRM_PASSWORD))
+
+        """ Open Affiliates page """
+        affiliate_list_view_page = CRMHomePage().open_more_list_modules().open_more_list_modules() \
+            .select_affiliates_module_more_list(AffiliateModuleConstants.AFFILIATES_MODULE)
+
+        '''Open popup and create new affiliate '''
+        return affiliate_list_view_page.open_create_affiliate_popup() \
+            .perform_create_affiliate(AffiliateModuleConstants.PARTNER_NAME,
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.BRAND_NEW_FOREX),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.ALLOWED_IP),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.IS_ENABLED),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.FIRST_ALLOWED_METHOD),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.SECOND_ALLOWED_METHOD),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.THIRD_ALLOWED_METHOD),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.FOURTH_ALLOWED_METHOD),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.FIFTH_ALLOWED_METHOD),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.FIRST_COUNTRY),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.SECOND_COUNTRY),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.THIRD_COUNTRY),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.FOURTH_COUNTRY),
+                                      Config.data.get_data_affliate_info(AffiliateModuleConstants.AFFILIATE_INFO,
+                                                                         AffiliateModuleConstants.FIFTH_COUNTRY))
+
