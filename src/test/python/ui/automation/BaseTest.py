39fee68b31f087b3907487f8de7923a0f6aedd47
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-07-26 15:07:18 +0300

    develop: all test were updated

--- src/test/python/ui/automation/BaseTest.py
+++ src/test/python/ui/automation/BaseTest.py
@@ -8,21 +8,21 @@ from src.main.python.utils.config import Config
 
 
 class BaseTest(unittest.TestCase):
-    selenium_grid_url = "http://localhost:5578/wd/hub/"
-    options = webdriver.ChromeOptions()
-    options.add_argument("--lang=en")
-    options.add_argument("--start-maximized")
-
-    browser = webdriver.Remote(desired_capabilities=options.to_capabilities(),
-                               command_executor=selenium_grid_url)
 
     def setUp(self):
+        global driver
         Config.data = DataProviders()
         allure.MASTER_HELPER.environment(BROWSER="CHROME", URL_BRAND=Config.url_client_area, URL_CRM=Config.url_crm)
+        selenium_grid_url = "http://localhost:5578/wd/hub/"
+        options = webdriver.ChromeOptions()
+        options.add_argument("--lang=en")
+        options.add_argument("--start-maximized")
+        driver = webdriver.Remote(desired_capabilities=options.to_capabilities(),
+                                  command_executor=selenium_grid_url)
 
     @property
     def get_driver(self):
-        return type(self).browser
+        return driver
 
     def tearDown(self):
         """Take a Screen-shot of the drive homepage, when it Failed."""
@@ -33,8 +33,8 @@ class BaseTest(unittest.TestCase):
                     file_name = 'C:/Users/Administrator/.jenkins/workspace/Regression New Forex Staging' \
                                 '/allure/results/failed_screenshot %s.png' % now
 
-                    self.browser.get_screenshot_as_file(file_name)
-                    allure.MASTER_HELPER.attach('failed_screenshot', self.browser.get_screenshot_as_png(),
+                    driver.get_screenshot_as_file(file_name)
+                    allure.MASTER_HELPER.attach('failed_screenshot', driver.get_screenshot_as_png(),
                                                 type=AttachmentType.PNG)
 
         #             self.browser.close()
