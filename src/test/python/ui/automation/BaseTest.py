01d9f2ff57aca58bcd61b02007073a2c5001d50a
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-07-26 14:48:00 +0300

    develop: refactoring of code

--- src/test/python/ui/automation/BaseTest.py
+++ src/test/python/ui/automation/BaseTest.py
@@ -6,23 +6,26 @@ from selenium import webdriver
 from src.main.python.ui.brand.model.data.providers.DataProviders import DataProviders
 from src.main.python.utils.config import Config
 
+browser = None
+
 
 class BaseTest(unittest.TestCase):
-    selenium_grid_url = "http://localhost:5578/wd/hub/"
-    options = webdriver.ChromeOptions()
-    options.add_argument("--lang=en")
-    options.add_argument("--start-maximized")
-
-    browser = webdriver.Remote(desired_capabilities=options.to_capabilities(),
-                               command_executor=selenium_grid_url)
 
     def setUp(self):
+        global browser
         Config.data = DataProviders()
         allure.MASTER_HELPER.environment(BROWSER="CHROME", URL_BRAND=Config.url_client_area, URL_CRM=Config.url_crm)
+        selenium_grid_url = "http://localhost:5578/wd/hub/"
+        options = webdriver.ChromeOptions()
+        options.add_argument("--lang=en")
+        options.add_argument("--start-maximized")
+
+        browser = webdriver.Remote(desired_capabilities=options.to_capabilities(),
+                                   command_executor=selenium_grid_url)
 
     @property
     def get_driver(self):
-        return type(self).browser
+        return browser
 
     def tearDown(self):
         """Take a Screen-shot of the drive homepage, when it Failed."""
@@ -33,12 +36,12 @@ class BaseTest(unittest.TestCase):
                     file_name = 'C:/Users/Administrator/.jenkins/workspace/Regression New Forex Staging' \
                                 '/allure/results/failed_screenshot %s.png' % now
 
-                    self.browser.get_screenshot_as_file(file_name)
-                    allure.MASTER_HELPER.attach('failed_screenshot', self.browser.get_screenshot_as_png(),
+                    browser.get_screenshot_as_file(file_name)
+                    allure.MASTER_HELPER.attach('failed_screenshot', browser.get_screenshot_as_png(),
                                                 type=AttachmentType.PNG)
 
-                    self.browser.close()
-                    self.browser.quit()
+                    browser.close()
+                    browser.quit()
         else:
-            self.browser.close()
-            self.browser.quit()
+            browser.close()
+            browser.quit()
