645531f46c6a475e6c46515d2d0f066f0cdefbb9
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-07-31 18:04:06 +0300

    1000: Create filters were updated

--- src/test/python/ui/automation/implementation/crm/tests_help_desk_module/Test_Help_Desk_Module.py
+++ src/test/python/ui/automation/implementation/crm/tests_help_desk_module/Test_Help_Desk_Module.py
@@ -8,104 +8,104 @@ from src.test.python.ui.automation.utils.preconditions.help_desk.HelpDeskPrecond
 
 class HelpDeskTest(BaseTest):
 
-    # def test_create_ticket(self):
-    #     CRMLoginPage().open_first_tab_page(Config.url_crm) \
-    #         .crm_login(Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.USER_NAME),
-    #                    Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.CRM_PASSWORD))
-    #
-    #     HelpDeskPrecondition().create_first_ticket()
-    #
-    #     detail_view_page_service_desk_module = HelpDeskPage().select_filter(
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_COLUMNS, HelpDeskConstants.FILTER_NAME)) \
-    #         .find_ticket_by_title(HelpDeskConstants.FIRST_TITTLE) \
-    #         .perform_search_ticket() \
-    #         .open_ticket_number()
-    #
-    #     tittle = detail_view_page_service_desk_module.get_title_text()
-    #     category = detail_view_page_service_desk_module.get_category_status_text()
-    #     ticket_status = detail_view_page_service_desk_module.get_ticket_status_text()
-    #     account_name = detail_view_page_service_desk_module.get_account_name()
-    #     description = detail_view_page_service_desk_module.get_description_text()
-    #     notes = detail_view_page_service_desk_module.get_notes_text()
-    #     priority = detail_view_page_service_desk_module.get_priority_text()
-    #
-    #     assert tittle == HelpDeskConstants.FIRST_TITTLE
-    #     assert category == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                       HelpDeskConstants.FIRST_CATEGORY)
-    #     assert ticket_status == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                            HelpDeskConstants.FIRST_STATUS)
-    #
-    #     assert account_name == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                           HelpDeskConstants.SECOND_RELATED_TO)
-    #
-    #     assert description == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                          HelpDeskConstants.FIRST_DESCRIPTION)
-    #
-    #     assert notes == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                    HelpDeskConstants.FIRST_NOTES)
-    #
-    #     assert priority == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                       HelpDeskConstants.FIRST_PRIORITY)
-    #
-    # def test_edit_ticket(self):
-    #     CRMLoginPage().open_first_tab_page(Config.url_crm) \
-    #         .crm_login(Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.USER_NAME),
-    #                    Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.CRM_PASSWORD))
-    #
-    #     HelpDeskPrecondition().create_first_ticket()
-    #
-    #     HelpDeskPage().select_filter(Config.data.get_data_help_desk(
-    #         HelpDeskConstants.HELP_DESK_COLUMNS, HelpDeskConstants.FILTER_NAME)) \
-    #         .find_ticket_by_title(HelpDeskConstants.FIRST_TITTLE) \
-    #         .perform_search_ticket() \
-    #         .click_edit_ticket_pencil().perform_edit_ticket(
-    #         HelpDeskConstants.FIRST_TITTLE,
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                        HelpDeskConstants.FIRST_RELATED_TO),
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                        HelpDeskConstants.FIRST_ASSIGNED_TO),
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                        HelpDeskConstants.FIRST_STATUS),
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                        HelpDeskConstants.FIRST_PRIORITY),
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                        HelpDeskConstants.FIRST_CATEGORY),
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                        HelpDeskConstants.FIRST_TICKET_SOURCE),
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                        HelpDeskConstants.FIRST_DESCRIPTION),
-    #         Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                        HelpDeskConstants.FIRST_NOTES))
-    #
-    #     detail_view_page_service_desk_module = HelpDeskPage().find_ticket_by_title(HelpDeskConstants.FIRST_TITTLE) \
-    #         .perform_search_ticket() \
-    #         .open_ticket_number()
-    #
-    #     tittle = detail_view_page_service_desk_module.get_title_text()
-    #     category = detail_view_page_service_desk_module.get_category_status_text()
-    #     ticket_status = detail_view_page_service_desk_module.get_ticket_status_text()
-    #     account_name = detail_view_page_service_desk_module.get_account_name()
-    #     description = detail_view_page_service_desk_module.get_description_text()
-    #     notes = detail_view_page_service_desk_module.get_notes_text()
-    #     priority = detail_view_page_service_desk_module.get_priority_text()
-    #
-    #     assert tittle == HelpDeskConstants.FIRST_TITTLE
-    #     assert category == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                       HelpDeskConstants.FIRST_CATEGORY)
-    #     assert ticket_status == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                            HelpDeskConstants.FIRST_STATUS)
-    #
-    #     assert account_name == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                           HelpDeskConstants.SECOND_RELATED_TO)
-    #
-    #     assert description == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                          HelpDeskConstants.FIRST_DESCRIPTION)
-    #
-    #     assert notes == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                    HelpDeskConstants.FIRST_NOTES)
-    #
-    #     assert priority == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
-    #                                                       HelpDeskConstants.FIRST_PRIORITY)
+    def test_create_ticket(self):
+        CRMLoginPage().open_first_tab_page(Config.url_crm) \
+            .crm_login(Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.USER_NAME),
+                       Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.CRM_PASSWORD))
+
+        HelpDeskPrecondition().create_first_ticket()
+
+        detail_view_page_service_desk_module = HelpDeskPage().select_filter(
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_COLUMNS, HelpDeskConstants.FILTER_NAME)) \
+            .find_ticket_by_title(HelpDeskConstants.FIRST_TITTLE) \
+            .perform_search_ticket() \
+            .open_ticket_number()
+
+        tittle = detail_view_page_service_desk_module.get_title_text()
+        category = detail_view_page_service_desk_module.get_category_status_text()
+        ticket_status = detail_view_page_service_desk_module.get_ticket_status_text()
+        account_name = detail_view_page_service_desk_module.get_account_name()
+        description = detail_view_page_service_desk_module.get_description_text()
+        notes = detail_view_page_service_desk_module.get_notes_text()
+        priority = detail_view_page_service_desk_module.get_priority_text()
+
+        assert tittle == HelpDeskConstants.FIRST_TITTLE
+        assert category == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                          HelpDeskConstants.FIRST_CATEGORY)
+        assert ticket_status == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                               HelpDeskConstants.FIRST_STATUS)
+
+        assert account_name == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                              HelpDeskConstants.SECOND_RELATED_TO)
+
+        assert description == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                             HelpDeskConstants.FIRST_DESCRIPTION)
+
+        assert notes == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                       HelpDeskConstants.FIRST_NOTES)
+
+        assert priority == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                          HelpDeskConstants.FIRST_PRIORITY)
+
+    def test_edit_ticket(self):
+        CRMLoginPage().open_first_tab_page(Config.url_crm) \
+            .crm_login(Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.USER_NAME),
+                       Config.data.get_data_client(TestDataConstants.CLIENT_ONE, TestDataConstants.CRM_PASSWORD))
+
+        HelpDeskPrecondition().create_first_ticket()
+
+        HelpDeskPage().select_filter(Config.data.get_data_help_desk(
+            HelpDeskConstants.HELP_DESK_COLUMNS, HelpDeskConstants.FILTER_NAME)) \
+            .find_ticket_by_title(HelpDeskConstants.FIRST_TITTLE) \
+            .perform_search_ticket() \
+            .click_edit_ticket_pencil().perform_edit_ticket(
+            HelpDeskConstants.FIRST_TITTLE,
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                           HelpDeskConstants.FIRST_RELATED_TO),
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                           HelpDeskConstants.FIRST_ASSIGNED_TO),
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                           HelpDeskConstants.FIRST_STATUS),
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                           HelpDeskConstants.FIRST_PRIORITY),
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                           HelpDeskConstants.FIRST_CATEGORY),
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                           HelpDeskConstants.FIRST_TICKET_SOURCE),
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                           HelpDeskConstants.FIRST_DESCRIPTION),
+            Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                           HelpDeskConstants.FIRST_NOTES))
+
+        detail_view_page_service_desk_module = HelpDeskPage().find_ticket_by_title(HelpDeskConstants.FIRST_TITTLE) \
+            .perform_search_ticket() \
+            .open_ticket_number()
+
+        tittle = detail_view_page_service_desk_module.get_title_text()
+        category = detail_view_page_service_desk_module.get_category_status_text()
+        ticket_status = detail_view_page_service_desk_module.get_ticket_status_text()
+        account_name = detail_view_page_service_desk_module.get_account_name()
+        description = detail_view_page_service_desk_module.get_description_text()
+        notes = detail_view_page_service_desk_module.get_notes_text()
+        priority = detail_view_page_service_desk_module.get_priority_text()
+
+        assert tittle == HelpDeskConstants.FIRST_TITTLE
+        assert category == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                          HelpDeskConstants.FIRST_CATEGORY)
+        assert ticket_status == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                               HelpDeskConstants.FIRST_STATUS)
+
+        assert account_name == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                              HelpDeskConstants.SECOND_RELATED_TO)
+
+        assert description == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                             HelpDeskConstants.FIRST_DESCRIPTION)
+
+        assert notes == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                       HelpDeskConstants.FIRST_NOTES)
+
+        assert priority == Config.data.get_data_help_desk(HelpDeskConstants.HELP_DESK_INFO,
+                                                          HelpDeskConstants.FIRST_PRIORITY)
 
     def test_searching_for_help_desk(self):
         CRMLoginPage().open_first_tab_page(Config.url_crm) \
