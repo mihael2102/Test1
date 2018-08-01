0a61280d246c7120538762e7d733ed78142a9ec4
yevhen_lyzohubov <evgeniy.l@pandats.com>
2018-08-01 18:00:43 +0300

    develop: all tests were added

--- src/main/python/ui/brand/model/forms/signup/BrandSignUpForm.py
+++ src/main/python/ui/brand/model/forms/signup/BrandSignUpForm.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from src.main.python.ui.brand.model.ca_base_page.BrandBasePage import BrandBasePage
 from src.main.python.ui.brand.model.pages.trading_platform.BrandTradingPlatformPage import BrandTradingPlatformPage
+from src.main.python.utils.logs.Loging import Logging
+
 
 class BrandSignUpForm(BrandBasePage):
 
@@ -11,46 +13,53 @@ class BrandSignUpForm(BrandBasePage):
         self.email_locator = self.driver.find_element(By.XPATH, "//input[@name='email']")
         self.email_locator.clear()
         self.email_locator.send_keys(email)
+        Logging().reportDebugStep(self, "The email was set: " + email)
         return BrandSignUpForm()
 
     def set_password(self, password):
         password_locator = self.driver.find_element(By.XPATH, "//*[@name='password']")
         password_locator.clear()
         password_locator.send_keys(password)
+        Logging().reportDebugStep(self, "The password was set: " + password)
         return BrandSignUpForm()
 
     def set_confirm_password(self, confirm_password):
         confirmLocator = self.driver.find_element(By.XPATH, "//*[@name='passwordConfirm']")
         confirmLocator.clear()
         confirmLocator.send_keys(confirm_password)
+        Logging().reportDebugStep(self, "The confirm password was set: " + confirm_password)
         return BrandSignUpForm()
 
     def set_promo_code(self, promo_code):
         promo_code_locator = self.driver.find_element(By.XPATH, "//*[@placeholder='Enter Promocode']")
         promo_code_locator.clear()
         promo_code_locator.send_keys(promo_code)
+        Logging().reportDebugStep(self, "The promo code was set: " + promo_code)
         return BrandSignUpForm()
 
     def set_country(self, country):
         change_country_locator = self.driver.find_element(By.XPATH,
-                                                               "//div[@class='country-pandats']//following-sibling::a")
+                                                          "//div[@class='country-pandats']//following-sibling::a")
         change_country_locator.click()
         exist_counter_locator = self.driver.find_element(By.XPATH, "//div[@class='content-pandats']//ul")
         search_locator = self.driver.find_element(By.XPATH,
-                                                       "//div[@class='search-pandats']//following-sibling::input")
+                                                  "//div[@class='search-pandats']//following-sibling::input")
 
         search_locator.send_keys(country)
         exist_counter_locator.click()
+        Logging().reportDebugStep(self, "The country was set: " + country)
         return BrandSignUpForm()
 
     def set_check_box(self):
-        checkBoxLocator = self.driver.find_element(By.XPATH, "//label[@class='terms-pandats']")
-        checkBoxLocator.click()
+        check_box_locator = self.driver.find_element(By.XPATH, "//label[@class='terms-pandats']")
+        check_box_locator.click()
+        Logging().reportDebugStep(self, "The check box was set")
         return BrandSignUpForm()
 
     def sign_up_button(self):
-        submitLocator = self.driver.find_element(By.XPATH,
-                                                      "//button[@class='forex-button-pandats signup-button-pandats']")
+        submit_locator = self.driver.find_element(By.XPATH,
+                                                 "//button[@class='forex-button-pandats signup-button-pandats']")
 
-        submitLocator.click()
+        submit_locator.click()
+        Logging().reportDebugStep(self, "The sign up button was clicked")
         return BrandTradingPlatformPage()
--- src/main/python/ui/brand/model/forms/trading_experience/BrandTradingExperienceForm.py
+++ src/main/python/ui/brand/model/forms/trading_experience/BrandTradingExperienceForm.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from src.main.python.ui.brand.model.ca_base_page.BrandBasePage import BrandBasePage
+from src.main.python.utils.logs.Loging import Logging
 
 
 class BrandTradingExperienceForm(BrandBasePage):
@@ -14,6 +15,7 @@ class BrandTradingExperienceForm(BrandBasePage):
         select = self.driver.find_element(By.XPATH, "//custom-select[@name= 'tradingFrequency']//"
                                                     "following-sibling::*[contains(text(),'%s')]" % trading_frequency)
         select.click()
+        Logging().reportDebugStep(self, "The trading_frequency was set: " + trading_frequency)
         return BrandTradingExperienceForm()
 
     def select_cfd_trading(self, cfd_trading):
@@ -22,6 +24,7 @@ class BrandTradingExperienceForm(BrandBasePage):
         select = self.driver.find_element(By.XPATH, "//custom-select[@name= 'cfdTrading']//"
                                                     "following-sibling::*[contains(text(),'%s')]" % cfd_trading)
         select.click()
+        Logging().reportDebugStep(self, "The cfd trading was set: " + cfd_trading)
         return BrandTradingExperienceForm()
 
     def select_otcd_frequency(self, otcd_frequency):
@@ -30,6 +33,7 @@ class BrandTradingExperienceForm(BrandBasePage):
         select = self.driver.find_element(By.XPATH, "//custom-select[@name= 'otcdFrequency']//"
                                                     "following-sibling::*[contains(text(),'%s')]" % otcd_frequency)
         select.click()
+        Logging().reportDebugStep(self, "The otcd_frequency was set: " + otcd_frequency)
         return BrandTradingExperienceForm()
 
     def select_etd_frequency(self, etd_frequency):
@@ -38,6 +42,7 @@ class BrandTradingExperienceForm(BrandBasePage):
         select = self.driver.find_element(By.XPATH, "//custom-select[@name= 'etdFrequency']//"
                                                     "following-sibling::*//*[contains(text(),'%s')]" % etd_frequency)
         select.click()
+        Logging().reportDebugStep(self, "The etd frequency was set: " + etd_frequency)
         return BrandTradingExperienceForm()
 
     def select_us_citizen(self, us_citizen):
@@ -46,15 +51,18 @@ class BrandTradingExperienceForm(BrandBasePage):
         select = self.driver.find_element(By.XPATH, "//custom-select[@name= 'usCitizen']//"
                                                     "following-sibling::*[contains(text(),'%s')]" % us_citizen)
         select.click()
+        Logging().reportDebugStep(self, "The eus_citizen was set: " + us_citizen)
         return BrandTradingExperienceForm()
 
     def set_us_taxid(self, us_taxid):
         field_us_taxid = self.driver.find_element(By.XPATH, "//input[@name='ustaxid']")
         field_us_taxid.clear()
         field_us_taxid.send_keys(us_taxid)
+        Logging().reportDebugStep(self, "The us_taxid was set: " + us_taxid)
         return BrandTradingExperienceForm()
 
     def enter_finish_button(self):
         next_button = self.driver.find_element(By.XPATH, "//div[@class='next-pandats']")
         next_button.click()
+        Logging().reportDebugStep(self, "Next button was clicked")
         return BrandTradingExperienceForm()
