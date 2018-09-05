02c32fe0f20f2f09545b71e6ac102580ba709670
dmytro.k <dmytro.k@pandats.com>
2018-09-05 17:03:55 +0300

    develop: files for changing name of Jenkin's job in path and specified ENV before execution were added

---
+++ Run_Change_Regression_Test.py
@@ -0,0 +1,23 @@
+import fileinput
+
+path_to_file = "C:/Users/Administrator/.jenkins/workspace/Regression New Forex Staging/src/main/python/utils/config/Config.py"
+
+test = "Regression New Forex Staging"
+
+collector = []
+
+with open(path_to_file, "a") as f:
+    for i, line in enumerate(fileinput.input(path_to_file)):
+        if line.startswith("test"):
+            tmp_list = line.split("\"")
+            tmp_list[1] = test
+            new_line = "\"".join(tmp_list)
+            collector.append(new_line)
+        else:
+            collector.append(line)
+
+open(path_to_file, 'w').close()
+
+with open(path_to_file, "a") as f:
+    for i in collector:
+        f.write(i)
