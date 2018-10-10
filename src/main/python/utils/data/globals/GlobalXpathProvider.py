6a3b7678ee9e0fd3886f0092d235cf3d2f8c0f69
dmytro.k <dmytro.k@pandats.com>
2018-10-10 15:18:05 +0300

    new_forex_updating_of_brands_xpath: - in case of brand xpath absence default xpath dictionary is loaded

--- src/main/python/utils/data/globals/GlobalXpathProvider.py
+++ src/main/python/utils/data/globals/GlobalXpathProvider.py
@@ -5,12 +5,14 @@ current_brand_name = None
 current_brand_xpath_dict = None
 
 
-def __get_xpath_of_elements_of_current_page(current_page_name):
+def __get_default_xpath_of_elements_of_current_page():
     # GET DEFAULT XPATH VALUES FOR ALL PAGES
     default_xpath_dict_for_all_pages = ConfigProvider().get_default_xpath_dictionary()
+    return default_xpath_dict_for_all_pages
 
+def __get_xpath_of_elements_of_current_page(current_page_name):
     # Get default xpath values for elements of current page
-    FinancialTransactionsPage_default_dict = default_xpath_dict_for_all_pages[current_page_name]
+    FinancialTransactionsPage_default_dict = __get_default_xpath_of_elements_of_current_page[current_page_name]
 
     # Get xpath values for elements of current brand. If in file there is no xpath for page, return default dict 'as is'
     # without any changes
@@ -29,6 +31,9 @@ def get_xpath_for_current_brand_element(page_name):
         if page_name == "FinancialTransactionsPage":
            return __get_xpath_of_elements_of_current_page(page_name)
 
-    if current_brand_name == "newforexstaging":
+    elif current_brand_name == "newforexstaging":
         if page_name == "FinancialTransactionsPage":
             return __get_xpath_of_elements_of_current_page(page_name)
+
+    else:
+        return __get_default_xpath_of_elements_of_current_page()[page_name]
