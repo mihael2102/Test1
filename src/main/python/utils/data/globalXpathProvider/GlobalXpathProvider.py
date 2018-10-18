35c64a787c9ffe373c27f028f59165e4fea937f2
dmytro.k <dmytro.k@pandats.com>
2018-10-18 13:17:42 +0300

    old_forex_updating_of_brands_xpath: - updating tests and pages - update XpathProvider

--- src/main/python/utils/data/globalXpathProvider/GlobalXpathProvider.py
+++ src/main/python/utils/data/globalXpathProvider/GlobalXpathProvider.py
@@ -3,11 +3,13 @@
 current_brand_name = None
 current_brand_xpath_dict = None
 
+
 def __get_default_xpath_of_elements_of_current_page():
     # GET DEFAULT XPATH VALUES FOR ALL PAGES
     from src.main.python.utils.data.providers.ConfigProvider import ConfigProvider
     default_xpath_dict_for_all_pages = ConfigProvider().get_default_xpath_dictionary()
     return default_xpath_dict_for_all_pages
+
 
 def __get_xpath_of_elements_of_current_page(current_page_name):
     # Get default xpath values for elements of current page
@@ -18,26 +20,27 @@ def __get_xpath_of_elements_of_current_page(current_page_name):
     try:
         from src.main.python.utils.data.providers.ConfigProvider import ConfigProvider
         FinancialTransactionsPage_mpcrypto_xpath_dict = ConfigProvider().get_xpath_for_brand_pages()[current_page_name]
-    except (TypeError, KeyError):
+    except (TypeError, KeyError, FileNotFoundError):
         return FinancialTransactionsPage_default_dict
 
     # Rewrite default xpath values with relevant xpath values of current brand
     FinancialTransactionsPage_default_dict.update(FinancialTransactionsPage_mpcrypto_xpath_dict)
     return FinancialTransactionsPage_default_dict
 
+
 # Add new brands and pages for brands here
 def get_xpath_for_current_brand_element(page_name):
 
-    if (current_brand_name == "triomarkets") or (current_brand_name == "ogtrade") or \
-       (current_brand_name == "finmarket") or (current_brand_name == "fxpmarkets") or \
-       (current_brand_name == "gmo") or (current_brand_name == "oinvestsa") or \
-       (current_brand_name == "rimarkets-staging") or (current_brand_name == "rimarkets") or \
-       (current_brand_name == "itrader") or (current_brand_name == "mlnfx"):
-        if page_name == "FinancialTransactionsPage":
-            return __get_xpath_of_elements_of_current_page(page_name)
-        if page_name == "LeadViewInfo":
-            return __get_xpath_of_elements_of_current_page(page_name)
-        if page_name == "ClientsPage":
-            return __get_xpath_of_elements_of_current_page(page_name)
-
-    return __get_default_xpath_of_elements_of_current_page()[page_name]
+    # if (current_brand_name == "triomarkets") or (current_brand_name == "ogtrade") or \
+    #    (current_brand_name == "finmarket") or (current_brand_name == "fxpmarkets") or \
+    #    (current_brand_name == "gmo") or (current_brand_name == "oinvestsa") or \
+    #    (current_brand_name == "rimarkets-staging") or (current_brand_name == "rimarkets") or \
+    #    (current_brand_name == "itrader") or (current_brand_name == "mlnfx"):
+    #     if page_name == "FinancialTransactionsPage":
+    return __get_xpath_of_elements_of_current_page(page_name)
+    #     if page_name == "LeadViewInfo":
+    #         return __get_xpath_of_elements_of_current_page(page_name)
+    #     if page_name == "ClientsPage":
+    #         return __get_xpath_of_elements_of_current_page(page_name)
+    #
+    # return __get_default_xpath_of_elements_of_current_page()[page_name]
