15b3a923bfc18e6feee538ace48f275f476a25d2
dmytro.k <dmytro.k@pandats.com>
2018-11-27 15:05:58 +0200

    NF_add_new_tests: add changes for server

--- src/test/python/ui/automation/implementation/crm/tests_financial_transactions/Test_Tab_Financial_Transaction_Module.py
+++ src/test/python/ui/automation/implementation/crm/tests_financial_transactions/Test_Tab_Financial_Transaction_Module.py
@@ -473,6 +473,7 @@ class TabFinancialTransaction(BaseTest):
             .select_financial_transactions_module_more_list(
             FinancialTransactionsModuleConstants.FINANCIAL_TRANSACTIONS_MODULE)
 
+
         if (global_var.current_brand_name == "4xfx") or (global_var.current_brand_name == "24btcmarket") or (global_var.current_brand_name == "kontofx"):
             financial_transaction_module.enter_client_name(CRMConstants.EASY_SEARCH_CLIENT_TEST)
         else:
