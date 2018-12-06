3d7148cbeda6747136fbe77d83fbe79c5eb026d7
dmytro.k <dmytro.k@pandats.com>
2018-12-06 10:59:10 +0200

    NF_add_new_report: changed for new report

--- src/main/python/ui/crm/model/mt4/MT4DropDown.py
+++ src/main/python/ui/crm/model/mt4/MT4DropDown.py
@@ -1,11 +1,12 @@
 from src.main.python.ui.crm.model.pages.crm_base_page.CRMBasePage import CRMBasePage
 import time
+from selenium.webdriver.common.by import By
 
 class MT4DropDown(CRMBasePage):
 
     def mt4_actions(self, module):
         time.sleep(5)
-        selected_module = super().wait_element_to_be_clickable(
+        selected_module = self.driver.find_element(By.XPATH,
             "//a[@class='webMnu act_btn btn btn-primary'][%s]" % module)
         time.sleep(1)
         self.driver.execute_script("arguments[0].click();", selected_module)
