1f0ffdf64bb7c43277f4517f2d1ef5fd671b3340
dmytro.k <dmytro.k@pandats.com>
2018-12-18 17:53:04 +0200

    OF: changes by 12-18

--- MultiRunner.py
+++ MultiRunner.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
         # Join all results in one excel
         all_excel = "C:/Program Files (x86)/Jenkins/workspace/Old forex job 1/result/final_file.xlsx"
         writer = EX('C:/Program Files (x86)/Jenkins/workspace/Old forex job 1/result/final_file.xlsx')
-        f_short_name = "result"
+
 
 
         all_file_frames = []
@@ -143,155 +143,32 @@ if __name__ == "__main__":
             tab = pd.read_excel(filename)
             all_file_frames.append(tab)
             all_frame = pd.concat(all_file_frames, axis=1)
-            # all_frame.to_excel('C:/Program Files (x86)/Jenkins/workspace/Old forex job 1/result/final_file.xlsx')
-            all_frame.to_excel(writer, f_short_name, index=False)
-            workbook = writer.book
-            worksheet = writer.sheets[f_short_name]
-            format1 = workbook.add_format({'bg_color': '#FFC7CE',
-                                           'font_color': '#9C0006'})
+            all_frame.to_excel('C:/Program Files (x86)/Jenkins/workspace/Old forex job 1/result/final_file.xlsx')
 
-            format2 = workbook.add_format({'bg_color': '#C4D79B',
-                                           'font_color': '#000000'})
-            worksheet.conditional_format(0, 0, 841, 10, {'type': 'text',
-                                                         'criteria': 'beginsWith',
-                                                         'value': 'PASS',
-                                                         'format': format2})
+        workbook = writer.book
+        worksheet = writer.book.worksheets()[0]
+        format1 = workbook.add_format({'bg_color': '#FFC7CE',
+                                       'font_color': '#9C0006'})
 
-            worksheet.conditional_format(0, 0, 841, 10, {'type': 'text',
-                                                         'criteria': 'beginsWith',
-                                                         'value': 'ERROR',
-                                                         'format': format1})
-            worksheet.set_row(2, None, None, {'level': 1, 'hidden': True})
-            for i in range(3, 33):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
+        format2 = workbook.add_format({'bg_color': '#C4D79B',
+                                       'font_color': '#000000'})
+        worksheet.conditional_format(0, 0, 841, 10, {'type': 'text',
+                                                     'criteria': 'beginsWith',
+                                                     'value': 'PASS',
+                                                     'format': format2})
 
-            worksheet.set_row(34, None, None, {'level': 1, 'hidden': True})
-            for i in range(35, 51):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(52, None, None, {'level': 1, 'hidden': True})
-            for i in range(53, 77):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(78, None, None, {'level': 1, 'hidden': True})
-            for i in range(79, 107):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(108, None, None, {'level': 1, 'hidden': True})
-            for i in range(109, 139):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(140, None, None, {'level': 1, 'hidden': True})
-            for i in range(141, 181):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(182, None, None, {'level': 1, 'hidden': True})
-            for i in range(183, 234):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(235, None, None, {'level': 1, 'hidden': True})
-            for i in range(236, 333):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(334, None, None, {'level': 1, 'hidden': True})
-            for i in range(335, 384):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(385, None, None, {'level': 1, 'hidden': True})
-            for i in range(386, 403):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(404, None, None, {'level': 1, 'hidden': True})
-            for i in range(405, 434):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(435, None, None, {'level': 1, 'hidden': True})
-            for i in range(436, 446):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(447, None, None, {'level': 1, 'hidden': True})
-            for i in range(448, 467):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(468, None, None, {'level': 1, 'hidden': True})
-            for i in range(469, 487):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(488, None, None, {'level': 1, 'hidden': True})
-            for i in range(489, 506):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(507, None, None, {'level': 1, 'hidden': True})
-            for i in range(508, 525):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(526, None, None, {'level': 1, 'hidden': True})
-            for i in range(527, 546):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(547, None, None, {'level': 1, 'hidden': True})
-            for i in range(548, 565):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(566, None, None, {'level': 1, 'hidden': True})
-            for i in range(567, 596):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(597, None, None, {'level': 1, 'hidden': True})
-            for i in range(598, 633):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(634, None, None, {'level': 1, 'hidden': True})
-            for i in range(635, 654):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(655, None, None, {'level': 1, 'hidden': True})
-            for i in range(656, 692):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(693, None, None, {'level': 1, 'hidden': True})
-            for i in range(694, 720):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(721, None, None, {'level': 1, 'hidden': True})
-            for i in range(722, 738):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(739, None, None, {'level': 1, 'hidden': True})
-            for i in range(740, 760):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(761, None, None, {'level': 1, 'hidden': True})
-            for i in range(762, 770):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(771, None, None, {'level': 1, 'hidden': True})
-            for i in range(772, 786):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(787, None, None, {'level': 1, 'hidden': True})
-            for i in range(788, 795):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(796, None, None, {'level': 1, 'hidden': True})
-            for i in range(797, 808):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(809, None, None, {'level': 1, 'hidden': True})
-            for i in range(810, 823):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
-            worksheet.set_row(824, None, None, {'level': 1, 'hidden': True})
-            for i in range(825, 839):
-                worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
-
+        worksheet.conditional_format(0, 0, 841, 10, {'type': 'text',
+                                                     'criteria': 'beginsWith',
+                                                     'value': 'ERROR',
+                                                     'format': format1})
+        worksheet.set_row(2, None, None, {'level': 1, 'hidden': True})
+        for i in range(3, 33):
+            worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
         writer.save()
-
-
-
 
         Send_ALL_XLS(all_excel)
 
+        # os.system('start allure generate D:/automation-newforexqa/result -o D:/automation-newforexqa/result/allure-result')
 
     else:
         print("TURN ON VPN")
