12727b79806ed195e93455108e32c5cb13da702f
annapoimenova <anna.p@pandats.com>
2019-02-07 14:25:42 +0200

    NF_API_v4: changed report

--- MultiRunner.py
+++ MultiRunner.py
@@ -173,6 +173,7 @@ if __name__ == "__main__":
                                                               'criteria': 'beginsWith',
                                                               'value': 'ERROR',
                                                               'format': format1})
+                worksheet.freeze_panes(1, 1)
                 worksheet.set_row(2, None, None, {'level': 1, 'hidden': True})
                 for i in range(3, 33):
                     worksheet.set_row(i, None, None, {'level': 2, 'hidden': True})
