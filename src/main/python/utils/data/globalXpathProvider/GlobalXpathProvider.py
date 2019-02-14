27e3d83a3377a185ed5b050a468f9d9be104bee1
Panda102 <michael.oryshchenko@pandats.com>
2019-02-14 13:44:40 +0200

    solocapitals fix

--- src/main/python/utils/data/globalXpathProvider/GlobalXpathProvider.py
+++ src/main/python/utils/data/globalXpathProvider/GlobalXpathProvider.py
@@ -100,7 +100,7 @@ def get_xpath_for_current_brand_element(page_name):
         elif page_name == "AffiliatePage":
             return __get_xpath_of_elements_of_current_page(page_name)
 
-    elif current_brand_name == "solocapitlas":
+    elif current_brand_name == "solocapitals":
         if page_name == "TasksPage":
             return __get_xpath_of_elements_of_current_page(page_name)
         elif page_name == "FinancialTransactionsPage":
