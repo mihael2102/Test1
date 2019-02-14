7f2a590f3f5a8548a05dda3793e212c0a73ffa46
Panda102 <michael.oryshchenko@pandats.com>
2019-02-14 13:05:58 +0200

    verify new account appear in crm

--- MultiRunner.py
+++ MultiRunner.py
@@ -164,12 +164,12 @@ if __name__ == "__main__":
 
                 format2 = workbook.add_format({'bg_color': '#C4D79B',
                                                'font_color': '#000000'})
-                worksheet.conditional_format(0, 0, 68, 200, {'type': 'text',
+                worksheet.conditional_format(0, 0, 111, 200, {'type': 'text',
                                                               'criteria': 'beginsWith',
                                                               'value': 'PASS',
                                                               'format': format2})
 
-                worksheet.conditional_format(0, 0, 68, 200, {'type': 'text',
+                worksheet.conditional_format(0, 0, 111, 200, {'type': 'text',
                                                               'criteria': 'beginsWith',
                                                               'value': 'ERROR',
                                                               'format': format1})
