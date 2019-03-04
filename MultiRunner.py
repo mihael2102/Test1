07761faf4e6a2628534bc9278f0fce0db14b5122
Panda102 <michael.oryshchenko@pandats.com>
2019-03-04 09:03:34 +0200

    comment of sending reports

--- MultiRunner.py
+++ MultiRunner.py
@@ -240,8 +240,8 @@ if __name__ == "__main__":
 
                 writer.save()
 
-        Send_ALL_XLS(all_excel)
-        Send_ALL_XLS(short_excel)
+        # Send_ALL_XLS(all_excel)
+        # Send_ALL_XLS(short_excel)
 
 
 
