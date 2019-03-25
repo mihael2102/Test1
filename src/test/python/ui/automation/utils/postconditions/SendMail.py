48a8a3f994dc77b16d9aeabb906895399d9b9275
Panda102 <michael.oryshchenko@pandats.com>
2019-03-25 12:12:59 +0200

    api v6

--- src/test/python/ui/automation/utils/postconditions/SendMail.py
+++ src/test/python/ui/automation/utils/postconditions/SendMail.py
@@ -10,9 +10,9 @@ def Send_Email_XML(filepath, content):
     fromaddr = "jonathan.albalak@pandats.com"
     # to = "Niv.s@pandats.com"
     # to = "ann.poimenova@gmail.com"
-    to = "michael.oryshchenko@pandats.com"
-    cc = "anna.p@pandats.com"
-    bcc = "anna.poimenova@globallogic.com"
+    to = "jonathan.albalak@pandats.com"
+    cc = "jonathan.albalak@pandats.com"
+    bcc = "jonathan.albalak@pandats.com"
     # instance of MIMEMultipart
     msg = MIMEMultipart('alternative')
 
@@ -88,9 +88,9 @@ def Send_Email_XML(filepath, content):
 def Send_Email_XLS(filepath):
 
     fromaddr = "jonathan.albalak@pandats.com"
-    to = "Niv.s@pandats.com"
+    # to = "Niv.s@pandats.com"
     # to = "michael.oryshchenko@pandats.com"
-    # to = "ann.poimenova@gmail.com"
+    to = "ann.poimenova@gmail.com"
     cc = "anna.p@pandats.com"
     bcc = "michael.oryshchenko@pandats.com"
     # instance of MIMEMultipart
@@ -154,9 +154,9 @@ def Send_Email_XLS(filepath):
 def Send_ALL_XLS(filepath):
 
     fromaddr = "jonathan.albalak@pandats.com"
-    to = "Niv.s@pandats.com"
+    # to = "Niv.s@pandats.com"
     # to = "michael.oryshchenko@pandats.com"
-    # to = "ann.poimenova@gmail.com"
+    to = "ann.poimenova@gmail.com"
     cc = "anna.p@pandats.com"
     bcc = "michael.oryshchenko@pandats.com"
     # instance of MIMEMultipart
