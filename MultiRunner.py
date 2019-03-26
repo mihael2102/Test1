40811ca84de3f660e3097f0711ecfa166193056a
annapoimenova <anna.p@pandats.com>
2019-03-26 14:37:53 +0200

    new branch for run

--- MultiRunner.py
+++ MultiRunner.py
@@ -123,9 +123,9 @@ if __name__ == "__main__":
         # Form input list where each parameter is filename of TestSuite file
         # input_list = [path_to_brands_suite_1, path_to_brands_suite_2,path_to_brands_suite_3,path_to_brands_suite_4,path_to_brands_suite_5,path_to_brands_suite_6,
         #               path_to_brands_suite_7,path_to_brands_suite_8,path_to_brands_suite_9,path_to_brands_suite_10,path_to_brands_suite_11,path_to_brands_suite_12]
-        input_list = [path_to_brands_suite_1]
+        input_list = [path_to_brands_suite_1, path_to_brands_suite_2]
         # Init multiprocess
-        pool = multiprocessing.Pool(processes=1)
+        pool = multiprocessing.Pool(processes=2)
 
         # Run Test Suites as separate processes
         pool.map(__simple_run, input_list)
