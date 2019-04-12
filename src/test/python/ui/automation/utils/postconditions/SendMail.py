22014b3fe716547e87b8f6ef738dea6dfa05e62e
annapoimenova <anna.p@pandats.com>
2019-04-12 18:03:47 +0300

    start new test for web trader

--- src/test/python/ui/automation/utils/postconditions/SendMail.py
+++ src/test/python/ui/automation/utils/postconditions/SendMail.py
@@ -88,8 +88,8 @@ def Send_Email_XML(filepath, content):
 def Send_Email_XLS(filepath):
 
     fromaddr = "jonathan.albalak@pandats.com"
-    # to = "Niv.s@pandats.com"
-    to = "ann.poimenova@gmail.com"
+    to = "Niv.s@pandats.com"
+    # to = "ann.poimenova@gmail.com"
     cc = "anna.p@pandats.com"
     bcc = "michael.oryshchenko@pandats.com"
     # instance of MIMEMultipart
@@ -153,8 +153,8 @@ def Send_Email_XLS(filepath):
 def Send_ALL_XLS(filepath):
 
     fromaddr = "jonathan.albalak@pandats.com"
-    # to = "Niv.s@pandats.com"
-    to = "ann.poimenova@gmail.com"
+    to = "Niv.s@pandats.com"
+    # to = "ann.poimenova@gmail.com"
     cc = "anna.p@pandats.com"
     bcc = "michael.oryshchenko@pandats.com"
     # instance of MIMEMultipart
