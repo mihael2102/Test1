efa1a40f97a1c684aad8d9eb63628928bbd47a70
annapoimenova <anna.p@pandats.com>
2019-04-15 20:24:41 +0300

    fixed test trading process

--- src/main/python/ui/ca/model/pages/login/WebTraderPage.py
+++ src/main/python/ui/ca/model/pages/login/WebTraderPage.py
@@ -11,10 +11,11 @@ import src.main.python.utils.data.globalXpathProvider.GlobalXpathProvider as glo
 class WebTraderPage(CRMBasePage):
 
     def select_asset(self):
-        forex = self.driver.find_element(By.XPATH,"//*[@id='platform']/panda-forex-trading-platform/div/div/div/div[1]/asset-list/div/perfect-scrollbar/div[1]/ul/li[2]/div/span")
-        forex.click()
-        crypto = self.driver.find_element(By.XPATH,"//*[@id='platform']/panda-forex-trading-platform/div/div/div/div[1]/asset-list/div/perfect-scrollbar/div[1]/ul/li[3]/div/span")
-        crypto.click()
+        if global_var.current_brand_name != "brokerxp":
+            forex = self.driver.find_element(By.XPATH,"//*[@id='platform']/panda-forex-trading-platform/div/div/div/div[1]/asset-list/div/perfect-scrollbar/div[1]/ul/li[2]/div/span")
+            forex.click()
+            crypto = self.driver.find_element(By.XPATH,"//*[@id='platform']/panda-forex-trading-platform/div/div/div/div[1]/asset-list/div/perfect-scrollbar/div[1]/ul/li[3]/div/span")
+            crypto.click()
         sleep(5)
         click_select_account = self.driver.find_element(By.XPATH,"//*[@id='platform']/panda-forex-trading-platform/div/div/div/div[1]/asset-list/div/perfect-scrollbar/div[1]/ul/li[3]/ul/li[2]/asset-item/div/div[2][contains(text(), 'BTCUSD')]")
         click_select_account.click()
