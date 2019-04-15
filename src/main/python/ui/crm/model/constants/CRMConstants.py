38a3c5897ed912f360a191a6bff11ea919ee976b
annapoimenova <anna.p@pandats.com>
2019-04-15 14:55:20 +0300

    start new test for web trader

--- src/main/python/ui/crm/model/constants/CRMConstants.py
+++ src/main/python/ui/crm/model/constants/CRMConstants.py
@@ -64,11 +64,11 @@ class CRMConstants(object):
     SECOND_DATE = datetime.now() + relativedelta(days=1, minutes=15)
     THIRD_DATE = datetime.now() + relativedelta(days=2, minutes=30)
     FOURTH_DATE = datetime.now() + relativedelta(days=3, minutes=45)
-    MT4_ACCOUNT_CREATED_SUCCESFULLY = "MT4 Account created successfully"
-    MT4_ACCOUNT_UPDATED_SUCCESFULLY = "MT4 Account updated successfully"
+    MT4_ACCOUNT_CREATED_SUCCESFULLY = "Account created successfully"
+    MT4_ACCOUNT_UPDATED_SUCCESFULLY = "Account updated successfully"
     TRADING_SERVER_LIVE = "server_live"
     WITHDRAW_SUCCESSFULLY = "MT4 Withdraw successfull"
-    DEPOSIT_SUCCESSFULLY = "Successful MT4 Deposit"
+    DEPOSIT_SUCCESSFULLY = "Successful Deposit"
     TITLE_OF_CLIENT_DEPOSIT_POPUP = "Client Deposit"
     PASSWORD_CHANGE = "Password was change successfully"
     CRM_CLIENT_AREA_PASSWORD_CHANGE = "Client Area Password changed successfully"
