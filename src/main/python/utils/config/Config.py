82fd75ea5e61a96c7b02fab65631174f43a86c3b
annapoimenova <anna.p@pandats.com>
2019-04-22 17:40:56 +0300

    fixed Config

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,3 +1,5 @@
+from requests import get
+
 test = "Old forex job 1"
 timeout = 4
 data = None
@@ -10,6 +12,10 @@ counter = 1
 logger = None
 handler = None
 
+ip = get('https://api.ipify.org').text
+print('My public IP address is: {}'.format(ip))
 
-# chrome_driver = "C:/Users/Panda102/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
-chrome_driver = "D:/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
+if ip == '35.158.30.212':
+    chrome_driver = "D:/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
+elif ip == '35.158.90.50':
+    chrome_driver = "C:/Users/Panda102/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
