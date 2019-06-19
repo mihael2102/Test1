b2342fed5757cd6853ee9829e8effa8d994988f9
Panda102 <michael.oryshchenko@pandats.com>
2019-06-19 18:07:27 +0300

    time update

---
+++ src/main/python/ui/crm/model/constants/MT4ModuleConstants.py
@@ -0,0 +1,14 @@
+import random
+from datetime import *
+from dateutil.relativedelta import relativedelta
+
+
+class MT4ModuleConstants(object):
+    CHECK_MT_PASSWORD = "Check MT Password"
+    CHANGE_MT_PASSWORD = "Change MT Password"
+    DEPOSIT_MODULE = "Deposit"
+    CURRENCY_BTC = "BTC"
+    GROUP_REAL = "real"
+    LEVERAGE_100 = "100"
+    LEVERAGE_50 = "50"
+
