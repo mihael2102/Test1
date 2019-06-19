aa29c39e4516e1e468adc6271717b02e9312aa7f
Panda102 <michael.oryshchenko@pandats.com>
2019-06-19 16:30:41 +0300

    update v10

--- MultiRunner.py
+++ MultiRunner.py
@@ -137,18 +137,18 @@ if __name__ == "__main__":
 
         import xlsxwriter
         # Join all results in one excel
-        all_excel = "C:/Program Files (x86)/Jenkins/workspace/API Old Forex/result/final_file.xlsx"
+        all_excel = Config.file_path_1
         # writer = EX('C:/Program Files (x86)/Jenkins/workspace/Old forex job 1/result/final_file.xlsx')
 
 
 
         all_file_frames = []
-        for filename in glob.glob('C:/Program Files (x86)/Jenkins/workspace/API Old Forex/result/*.xlsx'):
+        for filename in glob.glob(Config.file_path_2):
             if "test_results" in filename:
                 tab = pd.read_excel(filename)
                 all_file_frames.append(tab)
                 all_frame = pd.concat(all_file_frames, axis=1)
-                writer = EX('C:/Program Files (x86)/Jenkins/workspace/API Old Forex/result/final_file.xlsx')
+                writer = EX(Config.file_path_1)
                 all_frame.to_excel(writer, sheet_name='Sheet1')
                 workbook = writer.book
                 worksheet = writer.sheets['Sheet1']
@@ -209,16 +209,16 @@ if __name__ == "__main__":
                 writer.save()
 
         # Join all results in one excel
-        short_excel = "C:/Program Files (x86)/Jenkins/workspace/API Old Forex/result/short_final_file.xlsx"
+        short_excel = Config.short_excel_path
         # writer = EX('C:/Program Files (x86)/Jenkins/workspace/API Old Forex/result/final_file.xlsx')
 
         short_file_frames = []
-        for filename in glob.glob('C:/Program Files (x86)/Jenkins/workspace/API Old Forex/result/*.xlsx'):
+        for filename in glob.glob(Config.file_path_2):
             if "short_results" in filename:
                 tab = pd.read_excel(filename)
                 short_file_frames.append(tab)
                 short_frame = pd.concat(short_file_frames, axis=1)
-                writer = EX('C:/Program Files (x86)/Jenkins/workspace/API Old Forex/result/short_final_file.xlsx')
+                writer = EX(Config.short_excel_path)
                 short_frame.to_excel(writer, sheet_name='Sheet1')
                 workbook = writer.book
                 worksheet = writer.sheets['Sheet1']
