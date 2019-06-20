4a859d16d8b01e9cd4e99bbb74c3345c3a5351b1
Panda102 <michael.oryshchenko@pandats.com>
2019-06-20 13:12:52 +0300

    trade99 ca 2306

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,11 +1,11 @@
 from requests import get
 
-test = "New Forex CA"
-file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/New Forex CA/result/final_file.xlsx"
-file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/New Forex CA/result/*.xlsx'
-file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/New Forex CA/%s"
-short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/New Forex CA/result/short_final_file.xlsx"
-mail_subject = "New Forex CA"
+test = "Trade99 CA"
+file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CA/result/final_file.xlsx"
+file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/Trade99 CA/result/*.xlsx'
+file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CA/%s"
+short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CA/result/short_final_file.xlsx"
+mail_subject = "Trade99 CA"
 timeout = 4
 data = None
 url_client_area = ""
