27a67d49a2c1fe573eb55a350868257ea73718bd
Panda102 <michael.oryshchenko@pandats.com>
2019-06-20 13:02:44 +0300

    trade99 crm 2306

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,9 +1,9 @@
-test = "4XFX CRM"
-file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/4XFX CRM/result/final_file.xlsx"
-file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/4XFX CRM/result/*.xlsx'
-file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/4XFX CRM/%s"
-short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/4XFX CRM/result/short_final_file.xlsx"
-mail_subject = "4XFX CRM"
+test = "Trade99 CRM"
+file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CRM/result/final_file.xlsx"
+file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/Trade99 CRM/result/*.xlsx'
+file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CRM/%s"
+short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CRM/result/short_final_file.xlsx"
+mail_subject = "Trade99 CRM"
 timeout = 4
 data = None
 url_client_area = ""
