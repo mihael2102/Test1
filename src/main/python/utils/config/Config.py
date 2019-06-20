9e65e4ebf597e3f60270cced09353b8be916b53d
Panda102 <michael.oryshchenko@pandats.com>
2019-06-20 13:06:51 +0300

    nf sp crm 2306

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,9 +1,9 @@
-test = "Trade99 CRM"
-file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CRM/result/final_file.xlsx"
-file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/Trade99 CRM/result/*.xlsx'
-file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CRM/%s"
-short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/Trade99 CRM/result/short_final_file.xlsx"
-mail_subject = "Trade99 CRM"
+test = "NF Special CRM"
+file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/NF Special CRM/result/final_file.xlsx"
+file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/NF Special CRM/result/*.xlsx'
+file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/NF Special CRM/%s"
+short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/NF Special CRM/result/short_final_file.xlsx"
+mail_subject = "NF Special CRM"
 timeout = 4
 data = None
 url_client_area = ""
