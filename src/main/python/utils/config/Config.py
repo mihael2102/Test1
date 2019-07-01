559bd0a2e9e51336e6d053177cc2bc2864814ae3
Panda102 <michael.oryshchenko@pandats.com>
2019-07-01 14:16:04 +0300

    tests multi threads nf crm

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,9 +1,9 @@
-test = "New forex job 1"
-file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/New forex job 1/result/final_file.xlsx"
-file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/New forex job 1/result/*.xlsx'
-file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/New forex job 1/%s"
-short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/New forex job 1/result/short_final_file.xlsx"
-mail_subject = "Something"
+test = "MultithreadMultithread"
+file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/Multithread/result/final_file.xlsx"
+file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/Multithread/result/*.xlsx'
+file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/Multithread/%s"
+short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/Multithread/result/short_final_file.xlsx"
+mail_subject = "NF CRM multi threads"
 timeout = 4
 data = None
 url_client_area = ""
