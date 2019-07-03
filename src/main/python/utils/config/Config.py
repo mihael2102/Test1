3c5b419485904a087ac3728f5e8ccb4da18ee95d
Panda102 <michael.oryshchenko@pandats.com>
2019-07-03 11:05:03 +0300

    q8 ca

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,11 +1,11 @@
 from requests import get
 
-test = "New Forex CA"
-file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/New Forex CA/result/final_file.xlsx"
-file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/New Forex CA/result/*.xlsx'
-file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/New Forex CA/%s"
-short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/New Forex CA/result/short_final_file.xlsx"
-mail_subject = "New Forex CA"
+test = "Q8 CA"
+file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
+file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
+file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
+short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/short_final_file.xlsx"
+mail_subject = test
 timeout = 4
 data = None
 url_client_area = ""
