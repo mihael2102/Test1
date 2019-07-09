bde59a2bcedb0112b2481caae31df309f7697ffc
Panda102 <michael.oryshchenko@pandats.com>
2019-07-09 10:52:24 +0300

    debug

--- src/main/python/ui/crm/model/constants/CRMConstants.py
+++ src/main/python/ui/crm/model/constants/CRMConstants.py
@@ -15,7 +15,7 @@ class CRMConstants(object):
     DURATION_EVENT = "15M"
     SERVER_NOT_CONFIGURATE = "Server Not Configured?"
     CC_EMAIL = "jonathan.albalak@pandats.com"
-    SUBJECT_TASK_MAIL = "SUBJECT_TASK_MAIL" + str(random.randrange(1, 9999999))
+    SUBJECT_TASK_MAIL = ": SUBJECT_TASK_MAIL" + str(random.randrange(1, 9999999))
     TRADING_LEVERAGE_ITRADER = "30"
     SUBJECT_LEAD_MAIL = "Test MAIL" + str(random.randrange(1, 9999999))
     BODY_LEAD_MAIL = "Test"
