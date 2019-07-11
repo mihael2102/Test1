5947665df8e914187d42424a7555fe623c982586
Panda102 <michael.oryshchenko@pandats.com>
2019-07-11 15:36:00 +0300

    debug

--- src/test/python/ui/automation/utils/preconditions/workflows/WorkflowsPrecondition.py
+++ src/test/python/ui/automation/utils/preconditions/workflows/WorkflowsPrecondition.py
@@ -29,7 +29,8 @@ class WorkflowsPrecondition(object):
         self.config = config
 
     def create_workflows(self):
-        CRMLoginPage(self.driver).open_first_tab_page(self.config.get_value('url')) \
+        CRMLoginPage(self.driver)\
+            .open_first_tab_page(self.config.get_value('url')) \
             .crm_login(self.config.get_value(TestDataConstants.USER_NAME),
                        self.config.get_value(TestDataConstants.CRM_PASSWORD),
                        self.config.get_value(TestDataConstants.OTP_SECRET))
@@ -84,13 +85,14 @@ class WorkflowsPrecondition(object):
         assert name_workflow == WorkflowsConstants.NAME_WORKFLOW
 
     def check_workflow_by_status(self):
-        CRMLoginPage(self.driver).open_first_tab_page(self.config.get_value('url')) \
+        CRMLoginPage(self.driver)\
+            .open_first_tab_page(self.config.get_value('url')) \
             .crm_login(self.config.get_value(TestDataConstants.USER_NAME),
                        self.config.get_value(TestDataConstants.CRM_PASSWORD),
                        self.config.get_value(TestDataConstants.OTP_SECRET))
-        CRMHomePage(self.driver).open_client_module() \
-            .select_filter(self.config.get_value(
-                TestDataConstants.CLIENT_ONE, TestDataConstants.FILTER)) \
+        CRMHomePage(self.driver)\
+            .open_client_module() \
+            .select_filter(self.config.get_value(TestDataConstants.CLIENT_ONE, TestDataConstants.FILTER)) \
             .find_first_client_by_email(WorkflowsConstants.PANDATS_EMAIL)
         if global_var.current_brand_name == "q8" or global_var.current_brand_name == "trade99":
             ClientProfilePage(self.driver).change_client_status_with_pencil(WorkflowsConstants.STATUS_TEST)
@@ -107,13 +109,14 @@ class WorkflowsPrecondition(object):
         assert address == WorkflowsConstants.TEST_ADDRESS
 
     def check_workflow_by_country(self):
-        CRMLoginPage(self.driver).open_first_tab_page(self.config.get_value('url')) \
+        CRMLoginPage(self.driver)\
+            .open_first_tab_page(self.config.get_value('url')) \
             .crm_login(self.config.get_value(TestDataConstants.USER_NAME),
                        self.config.get_value(TestDataConstants.CRM_PASSWORD),
                        self.config.get_value(TestDataConstants.OTP_SECRET))
-        CRMHomePage(self.driver).open_client_module() \
-            .select_filter(self.config.get_value(
-            TestDataConstants.CLIENT_ONE, TestDataConstants.FILTER)) \
+        CRMHomePage(self.driver)\
+            .open_client_module() \
+            .select_filter(self.config.get_value(TestDataConstants.CLIENT_ONE, TestDataConstants.FILTER)) \
             .find_second_client_by_email(WorkflowsConstants.PANDATS_EMAIL)
         ClientProfilePage(self.driver).click_edit_personal_detail()
         ClientProfilePage(self.driver).select_country(WorkflowsConstants.COUNTRY_AUSTRIA)
@@ -127,7 +130,8 @@ class WorkflowsPrecondition(object):
         assert address == WorkflowsConstants.TEST_ADDRESS
 
     def delete_workflow(self):
-        CRMLoginPage(self.driver).open_first_tab_page(self.config.get_value('url')) \
+        CRMLoginPage(self.driver)\
+            .open_first_tab_page(self.config.get_value('url')) \
             .crm_login(self.config.get_value(TestDataConstants.USER_NAME),
                        self.config.get_value(TestDataConstants.CRM_PASSWORD),
                        self.config.get_value(TestDataConstants.OTP_SECRET))
