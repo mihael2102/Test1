cc6660bae76f373dab9100e08a63035f7a6b1a50
Panda102 <michael.oryshchenko@pandats.com>
2019-07-11 11:38:55 +0300

    stratton questionnaire tests

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,6 +1,6 @@
 from requests import get
 
-test = "New Forex CA"
+test = "Stratton Questionnaire"
 file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
 file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
 file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
