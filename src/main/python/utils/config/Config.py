53793135430b8f1908185599266841e40121df99
Panda102 <michael.oryshchenko@pandats.com>
2019-07-17 09:21:19 +0300

    brokerxp crm multi

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,9 +1,9 @@
 test = "Brokerxp CRM"
-file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/Brokerxp CRM/result/final_file.xlsx"
-file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/Brokerxp CRM/result/*.xlsx'
-file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/Brokerxp CRM/%s"
-short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/Brokerxp CRM/result/short_final_file.xlsx"
-mail_subject = "Brokerxp CRM"
+file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
+file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
+file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
+short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/short_final_file.xlsx"
+mail_subject = test
 timeout = 4
 data = None
 url_client_area = ""
