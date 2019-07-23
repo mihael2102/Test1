d9fb815ee440a0930edb557375ed3656cfc2d5fd
Panda102 <michael.oryshchenko@pandats.com>
2019-07-23 16:21:03 +0300

    debug

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,6 +1,6 @@
 from requests import get
 
-test = "New Forex CA"
+test = "NF Special CA"
 file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
 file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
 file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
@@ -19,8 +19,8 @@ logger = None
 handler = None
 
 
-# chrome_driver = "C:/Users/Panda102/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
-chrome_driver = "C:/Users/Panda102/Desktop/drivers/chromedriver2.exe"
+chrome_driver = "C:/Users/Panda102/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
+# chrome_driver = "C:/Users/Panda102/Desktop/drivers/chromedriver2.exe"
 
 
 # ip = get('https://api.ipify.org').text
