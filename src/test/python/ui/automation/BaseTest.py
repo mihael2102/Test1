3c55800039513fb861aeda7ac9f2533e9c1bd8cf
Panda102 <michael.oryshchenko@pandats.com>
2019-07-23 14:46:06 +0300

    debug

--- src/test/python/ui/automation/BaseTest.py
+++ src/test/python/ui/automation/BaseTest.py
@@ -24,13 +24,13 @@ class BaseTest(unittest.TestCase):
             self.config = ConfigProvider()
         #allure.MASTER_HELPER.environment(BROWSER="CHROME", URL_BRAND=Config.url_client_area, URL_CRM=Config.url_crm)
         if self.driver_type is None or self.driver_type == 'Chrome':
-            #for hidden browser
+            # for hidden browser
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument("user-agent=[panda_bot]")
             chrome_options.add_argument("--headless")
             chrome_options.add_argument("--window-size=1920x1080")
             self.driver = webdriver.Chrome(Config.chrome_driver, chrome_options=chrome_options)
-            #for opened chrome
+            # for opened chrome
             # opts = Options()
             # opts.add_argument("user-agent=[panda_bot]")
             # self.driver = webdriver.Chrome(chrome_options=opts, executable_path=Config.chrome_driver)
