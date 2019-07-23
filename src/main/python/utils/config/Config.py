451e5d42124b0cc418c60c9a6da70d3b3a1a95b1
Panda102 <michael.oryshchenko@pandats.com>
2019-07-23 17:59:08 +0300

    debug

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,9 +1,9 @@
 test = "Old Forex CA"
-file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/Old Forex CA/result/final_file.xlsx"
-file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/Old Forex CA/result/*.xlsx'
-file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/Old Forex CA/%s"
-short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/Old Forex CA/result/short_final_file.xlsx"
-mail_subject = "Old Forex CA"
+file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
+file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
+file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
+short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/short_final_file.xlsx"
+mail_subject = test
 timeout = 4
 data = None
 url_client_area = ""
@@ -16,7 +16,5 @@ logger = None
 handler = None
 
 
-
-
-# chrome_driver = "D:/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
-chrome_driver = "C:/Users/Panda102/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver2.exe"
+chrome_driver = "C:/Users/Panda102/Desktop/drivers/chromedriver2.exe"
+# chrome_driver = "C:/Users/Panda102/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
