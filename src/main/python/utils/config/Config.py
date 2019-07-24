1cb192a8890bbb30c023be6e917f74f0959e80bf
Panda102 <michael.oryshchenko@pandats.com>
2019-07-24 18:06:24 +0300

    debug

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,9 +1,9 @@
 test = "OF Sanity"
-file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/OF Sanity/result/final_file.xlsx"
-file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/OF Sanity/result/*.xlsx'
-file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/OF Sanity/%s"
-short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/OF Sanity/result/short_final_file.xlsx"
-mail_subject = "OF Sanity"
+file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
+file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
+file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
+short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/short_final_file.xlsx"
+mail_subject = test
 timeout = 4
 data = None
 url_client_area = ""
