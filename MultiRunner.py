25aacfb86be08665f2c080bdeb7726e9084aa4c7
Panda102 <michael.oryshchenko@pandats.com>
2019-08-22 13:56:21 +0300

    debug

--- MultiRunner.py
+++ MultiRunner.py
@@ -102,8 +102,7 @@ if __name__ == "__main__":
     ip = get('https://api.ipify.org').text
     print('My public IP address is: {}'.format(ip))
 
-    if ip == '35.158.30.212' or ip == '35.158.90.50':
-
+    if ip == '35.158.30.212' or ip == '35.158.90.50' or ip == '3.123.82.121':
 
         all_brands_yml = "brands.yml"
         path_to_brands_suite_1 = "brands1.yml"
