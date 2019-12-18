00ded828a791b9336163d5df2b4f4eb9c926f893
Panda102 <michael.oryshchenko@pandats.com>
2019-12-18 10:30:54 +0200

    Merge remote-tracking branch 'remotes/origin/NF_v20' into Login_v20
    # Conflicts:
    #	src/test/python/resources/config/brands1.yml

--- src/main/python/ui/ca/model/pages/login/CALoginPage.py
+++ src/main/python/ui/ca/model/pages/login/CALoginPage.py
@@ -307,6 +307,9 @@ class CALoginPage(CRMBasePage):
                 self.__class__.__name__)["login_btn"])
             self.driver.execute_script("arguments[0].click();", login_button)
         except(NoSuchElementException, TimeoutException):
+            sleep(1)
+            self.refresh_page()
+            sleep(1)
             login_button = super().wait_load_element(global_var.get_xpath_for_current_brand_element(
                 self.__class__.__name__)["login_btn"], timeout=35)
             self.driver.execute_script("arguments[0].click();", login_button)
