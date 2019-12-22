b73de9bf50643fc0326408213ad7900d689152f9
Panda102 <michael.oryshchenko@pandats.com>
2019-12-22 13:19:28 +0200

    debug

--- src/test/python/ui/automation/BaseTest.py
+++ src/test/python/ui/automation/BaseTest.py
@@ -1,9 +1,11 @@
 import unittest
 from datetime import *
-
 import allure
+import os
+import shutil
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+from allure.constants import AttachmentType
 from src.main.python.utils.config import Config
 from src.main.python.utils.data.providers.ConfigProvider import ConfigProvider
 from src.main.python.utils.logs.Loging import Logging
@@ -24,10 +26,13 @@ class BaseTest(unittest.TestCase):
         allure.MASTER_HELPER.environment(BROWSER="CHROME", URL_BRAND=Config.url_client_area, URL_CRM=Config.url_crm)
         if self.driver_type is None or self.driver_type == 'Chrome':
             ' OPENED BROWSER: '
-            self.driver = webdriver.Chrome(Config.chrome_driver)
+            opts = Options()
+            opts.add_argument(Config.user_agent)
+            self.driver = webdriver.Chrome(chrome_options=opts, executable_path=Config.chrome_driver)
             self.driver.maximize_window()
             ' CLOSED BROWSER: '
             # chrome_options = webdriver.ChromeOptions()
+            # chrome_options.add_argument(Config.user_agent)
             # chrome_options.add_argument("--headless")
             # chrome_options.add_argument("--window-size=1920x1080")
             # self.driver = webdriver.Chrome(Config.chrome_driver, chrome_options=chrome_options)
@@ -45,16 +50,19 @@ class BaseTest(unittest.TestCase):
 
     def tearDown(self):
         """Take a Screen-shot of the drive homepage, when it Failed."""
+        dir = 'C:/screenshots/%s' % Config.test
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
         if self._outcome.errors:
             for method, error in self._outcome.errors:
                 if error:
                     now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
                     print("%s %s" % (now, error))
-                    # file_name = 'C:/Users/Administrator/.jenkins/workspace/Regression New Forex Staging' \
-                    #             '/allure/results/failed_screenshot %s.png' % now
-                    #
-                    # driver.get_screenshot_as_file(file_name)
-                    # allure.MASTER_HELPER.attach('failed_screenshot', driver.get_screenshot_as_png(),
+                    os.makedirs(dir)
+                    file_name = Config.screenshot_path
+                    self.driver.get_screenshot_as_file(file_name)
+                    self.driver.save_screenshot(file_name)
+                    # allure.MASTER_HELPER.attach('failed_screenshot', self.driver.get_screenshot_as_png(),
                     #                             type=AttachmentType.PNG)
         sleep(3)
         self.driver.close()
