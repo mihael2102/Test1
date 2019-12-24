b43f94db1e5b534df7cfd84147d2861f8ca0dd02
Panda102 <michael.oryshchenko@pandats.com>
2019-12-24 14:37:37 +0200

    add ca deposit page test

--- src/main/python/ui/ca/model/pages/ca/CADepositPage.py
+++ src/main/python/ui/ca/model/pages/ca/CADepositPage.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from src.main.python.ui.crm.model.pages.crm_base_page.CRMBasePage import CRMBasePage
 from src.main.python.ui.crm.model.pages.main.ClientsPage import ClientsPage
 from src.main.python.utils.logs.Loging import Logging
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, NoSuchFrameException, TimeoutException
 from time import sleep
 import pyotp
 from selenium.webdriver.support.select import Select
@@ -17,17 +17,26 @@ class CADepositPage(CRMBasePage):
 
     def click_deposit_btn(self):
         sleep(0.1)
-        deposit_btn = super().wait_load_element(global_var.get_xpath_for_current_brand_element(
-                                                    self.__class__.__name__)["deposit_btn"])
-        self.driver.execute_script("arguments[0].click();", deposit_btn)
-        Logging().reportDebugStep(self, "Click DEPOSIT button")
+        try:
+            deposit_btn = super().wait_load_element(global_var.get_xpath_for_current_brand_element(
+                                                        self.__class__.__name__)["deposit_btn"])
+            self.driver.execute_script("arguments[0].click();", deposit_btn)
+            Logging().reportDebugStep(self, "Click DEPOSIT button")
+        except(NoSuchElementException, TimeoutException):
+            Logging().reportDebugStep(self, "DEPOSIT pop up is already opened")
         return CADepositPage(self.driver)
 
     def check_deposit_page_loaded(self):
         sleep(0.1)
-        if global_var.current_brand_name == "q8":
-            self.driver.switch_to_frame(self.driver.find_element(By.XPATH, "//*[@id='cke_1_contents']/iframe"))
+        ''' Check iframe is loaded '''
+        try:
+            self.driver.switch_to_frame(super().wait_load_element(global_var.get_xpath_for_current_brand_element(
+                                                            self.__class__.__name__)["deposit_iframe"]))
+            Logging().reportDebugStep(self, "DEPOSIT iframe was loaded successfully")
+        except(NoSuchFrameException, NoSuchElementException, TimeoutException):
+            Logging().reportDebugStep(self, "DEPOSIT iframe is not exist in current brand")
+        ''' Check any deposit button in iframe is available '''
         super().wait_load_element(global_var.get_xpath_for_current_brand_element(
                                                     self.__class__.__name__)["deposit_page"])
-        Logging().reportDebugStep(self, "DEPOSIT page was loaded successfully")
+        Logging().reportDebugStep(self, "DEPOSIT button in iframe is available")
         return CADepositPage(self.driver)
