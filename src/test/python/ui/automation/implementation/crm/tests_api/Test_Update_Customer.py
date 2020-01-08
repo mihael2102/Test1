58f9bf0e929257e623ca9750d1dead449372759b
Panda102 <michael.oryshchenko@pandats.com>
2020-01-08 16:18:45 +0200

    debug

--- src/test/python/ui/automation/implementation/crm/tests_api/Test_Update_Customer.py
+++ src/test/python/ui/automation/implementation/crm/tests_api/Test_Update_Customer.py
@@ -2,6 +2,7 @@ import pytest
 from src.test.python.ui.automation.utils.preconditions.api.Api_Preconditions import ApiPrecondition
 from src.test.python.ui.automation.BaseTest import *
 
+
 class UpdateCustomer(BaseTest):
 
     def test_update_customer(self):
