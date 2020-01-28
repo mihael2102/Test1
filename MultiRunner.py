3b62d3e109f59b3e7fa131d0e302d1b27010a306
Panda102 <michael.oryshchenko@pandats.com>
2020-01-28 14:45:28 +0200

    load test

--- MultiRunner.py
+++ MultiRunner.py
@@ -192,10 +192,23 @@ if __name__ == "__main__":
                       path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
                       path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
                       path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
-                      path_to_test_suite_1, path_to_test_suite_1]
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1,
+                      path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1, path_to_test_suite_1
+                      ]
 
         # Init multiprocess
-        pool = multiprocessing.Pool(processes=50)
+        pool = multiprocessing.Pool(processes=100)
 
         # Run Test Suites as separate processes
         pool.map(__simple_run, input_list)
