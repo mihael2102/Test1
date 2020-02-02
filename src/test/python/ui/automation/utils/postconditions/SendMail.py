6a4c0fc955da0222951173d30a76c5ed813113ce
Panda102 <michael.oryshchenko@pandats.com>
2020-02-02 15:04:58 +0200

    Merge branch 'NF_v20' into NF_Client_Deposit
    # Conflicts:
    #	src/test/python/resources/config/tests_ca.yml

--- src/test/python/ui/automation/utils/postconditions/SendMail.py
+++ src/test/python/ui/automation/utils/postconditions/SendMail.py
@@ -93,7 +93,7 @@ def Send_Email_XML(filepath, content):
 def Send_Email_XLS(filepath):
 
     fromaddr = Config.email_address
-    to = "michael.oryshchenko@pandats.com"
+    to = Config.email_address
     # to = "yarin.b@pandats.com"
     cc = ""
     bcc = ""
@@ -158,10 +158,10 @@ def Send_Email_XLS(filepath):
 def Send_ALL_XLS(filepath):
 
     fromaddr = Config.email_address
-    to = "Niv.s@pandats.com"
+    to = Config.email_address
     # to = "michael.oryshchenko@pandats.com"
-    cc = "yarin.b@pandats.com"
-    bcc = "michael.oryshchenko@pandats.com"
+    cc = ""
+    bcc = ""
     # instance of MIMEMultipart
     msg = MIMEMultipart('alternative')
 
