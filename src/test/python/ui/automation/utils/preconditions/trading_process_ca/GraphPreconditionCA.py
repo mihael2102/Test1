90122c167f2038151ff9579f232a2434d4cddc91
Panda102 <michael.oryshchenko@pandats.com>
2020-02-03 15:50:41 +0200

    add graph test to hourly test set

---
+++ src/test/python/ui/automation/utils/preconditions/trading_process_ca/GraphPreconditionCA.py
@@ -0,0 +1,52 @@
+from src.main.python.utils.config import Config
+from src.main.python.ui.crm.model.constants.TestDataConstants import TestDataConstants
+from src.main.python.ui.crm.model.constants.LeadsModuleConstants import LeadsModuleConstants
+import src.main.python.utils.data.globalXpathProvider.GlobalXpathProvider as global_var
+from src.main.python.ui.ca.model.pages.login.CALoginPage import CALoginPage
+from src.main.python.ui.ca.model.constants.CAconstants.CAConstants import CAConstants
+from src.main.python.ui.ca.model.pages.login.WebTraderPage import WebTraderPage
+from src.main.python.ui.ca.model.pages.ca.CAMainMenuPage import CAMainMenuPage
+from src.main.python.ui.ca.model.constants.CAconstants.TradingConstants import TradingConstants
+
+
+class GraphPreconditionCA(object):
+
+    driver = None
+    config = None
+
+    def __init__(self, driver, config):
+        self.driver = driver
+        self.config = config
+
+    def load_lead_from_config(self, lead_key):
+        lead = self.config.get_value(lead_key)
+        return lead
+
+    def verify_graph_ca(self):
+        CALoginPage(self.driver) \
+            .open_first_tab_page(self.config.get_value('url_ca'))\
+            .login() \
+            .enter_email(self.config.get_value('email_live_acc')) \
+            .enter_password(self.config.get_value('password_live_acc')) \
+            .click_login()\
+            .verify()
+
+        WebTraderPage(self.driver) \
+            .open_trading_page() \
+            .check_chart_loaded() \
+            .open_graph_tab(TradingConstants.GRAPH_TAB_5MIN) \
+            .check_chart_loaded()\
+            .open_graph_tab(TradingConstants.GRAPH_TAB_15MIN) \
+            .check_chart_loaded() \
+            .open_graph_tab(TradingConstants.GRAPH_TAB_30MIN) \
+            .check_chart_loaded() \
+            .open_graph_tab(TradingConstants.GRAPH_TAB_HOURLY) \
+            .check_chart_loaded() \
+            .open_graph_tab(TradingConstants.GRAPH_TAB_4HOURS) \
+            .check_chart_loaded() \
+            .open_graph_tab(TradingConstants.GRAPH_TAB_DAILY) \
+            .check_chart_loaded() \
+            .open_graph_tab(TradingConstants.GRAPH_TAB_WEEKLY) \
+            .check_chart_loaded() \
+            .open_graph_tab(TradingConstants.GRAPH_TAB_MONTHLY) \
+            .check_chart_loaded()
