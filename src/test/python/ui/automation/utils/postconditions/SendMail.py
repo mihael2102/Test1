0da9e93a0e6db7bec879f8ae5818dea7da10f236
Panda102 <michael.oryshchenko@pandats.com>
2020-02-17 17:24:18 +0200

    debug

--- src/test/python/ui/automation/utils/postconditions/SendMail.py
+++ src/test/python/ui/automation/utils/postconditions/SendMail.py
@@ -11,7 +11,7 @@ def Send_Email_XML(filepath, content):
 
     fromaddr = Config.email_address
     to = "israel.a@pandats.com"
-    cc = "michael.oryshchenko@pandats.com, Niv.s@pandats.com"
+    cc = "michael.oryshchenko@pandats.com, yarin.b@pandats.com"
     bcc = "uria@pandats.com"
     # instance of MIMEMultipart
     msg = MIMEMultipart('alternative')
@@ -94,7 +94,6 @@ def Send_Email_XLS(filepath):
 
     fromaddr = Config.email_address
     to = Config.email_address
-    # to = "yarin.b@pandats.com"
     cc = ""
     bcc = ""
     # instance of MIMEMultipart
@@ -158,10 +157,9 @@ def Send_Email_XLS(filepath):
 def Send_ALL_XLS(filepath):
 
     fromaddr = Config.email_address
-    to = "Niv.s@pandats.com"
-    # to = "michael.oryshchenko@pandats.com"
+    to = "michael.oryshchenko@pandats.com"
     cc = "yarin.b@pandats.com"
-    bcc = "michael.oryshchenko@pandats.com"
+    bcc = ""
     # instance of MIMEMultipart
     msg = MIMEMultipart('alternative')
 
