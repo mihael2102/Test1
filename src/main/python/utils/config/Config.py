9d084d4dc47b2f7d2b18f95f608c70ab1362374e
Panda102 <michael.oryshchenko@pandats.com>
2020-03-03 14:09:58 +0200

    Merge branch 'NF_v20' into Newforexstage2_CRM_v20

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,4 +1,4 @@
-test = "Newforexstage2 CRM"
+test = "Newforexqa2 CRM"
 file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
 file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
 file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
