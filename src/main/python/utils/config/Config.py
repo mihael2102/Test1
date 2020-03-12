448c87c3f8893653191c66adce165a480e8c749a
Panda102 <michael.oryshchenko@pandats.com>
2020-03-12 12:26:16 +0200

    Merge branch 'NF_v20' into Dax-300_CRM_v20
    # Conflicts:
    #	src/main/python/utils/config/Config.py
    #	src/test/python/resources/config/brands1.yml

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,9 +1,9 @@
-test = "DAX-300 CRM"
+test = "PMT247 CRM"
 file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
 file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
 file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
 short_excel_path = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/short_final_file.xlsx"
-screenshot_path = "C:/screenshots/" + test + "/scr.png"
+# screenshot_path = "C:/screenshots/" + test + "/scr.png"
 mail_subject = test
 test_list = "tests.yml"
 user_agent = "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) " \
