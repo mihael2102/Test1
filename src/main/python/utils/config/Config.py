fed2204782cb29ec541b7c0bf6d78dd547b79611
michael_oryshchenko <michael_oryshchenko@pandats.com>
2020-04-07 15:39:10 +0300

    add add_interaction ui test

--- src/main/python/utils/config/Config.py
+++ src/main/python/utils/config/Config.py
@@ -1,4 +1,4 @@
-test = "New forex CRM"
+test = "NewBrand CRM"
 file_path_1 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/result/final_file.xlsx"
 file_path_2 = 'C:/Program Files (x86)/Jenkins/workspace/' + test + '/result/*.xlsx'
 file_path_3 = "C:/Program Files (x86)/Jenkins/workspace/" + test + "/%s"
@@ -23,6 +23,6 @@ handler = None
 
 
 # chrome_driver = "C:/Users/Panda102/Desktop/drivers/chromedriver2.exe"
-# chrome_driver = "C:/Users/Panda102/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
-chrome_driver = \
-    "C:/Users/pandaTemp/PycharmProjects/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver2.exe"
+chrome_driver = "C:/Users/Panda102/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver.exe"
+# chrome_driver = \
+#     "C:/Users/pandaTemp/PycharmProjects/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver2.exe"
