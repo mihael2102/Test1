5433136f28336149c7bd07d53fe786c6f273dae1
michael_oryshchenko <michael_oryshchenko@pandats.com>
2020-04-16 11:10:53 +0300

    new ui test delete user

--- src/test/python/ui/automation/implementation/crm/tests_user_management_ui/TestUserManagementUI.py
+++ src/test/python/ui/automation/implementation/crm/tests_user_management_ui/TestUserManagementUI.py
@@ -2,6 +2,8 @@ import pytest
 from src.test.python.ui.automation.BaseTest import *
 from src.test.python.ui.automation.utils.preconditions.user_management_ui.CreateUserPreconditionUI import \
     CreateUserPreconditionUI
+from src.test.python.ui.automation.utils.preconditions.user_management_ui.DeleteUserPreconditionUI import \
+    DeleteUserPreconditionUI
 
 
 @pytest.mark.run(order=3)
@@ -9,3 +11,6 @@ class TestUserManagementUI(BaseTest):
 
     def test_create_user_ui(self):
         CreateUserPreconditionUI(self.driver, self.config).create_user_ui()
+
+    def test_delete_user_ui(self):
+        DeleteUserPreconditionUI(self.driver, self.config).delete_user_ui()
