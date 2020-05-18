366fc8c0a5a43f61eab2313ff4c2f7b2a2e9a59c
michael_oryshchenko <michael_oryshchenko@pandats.com>
2020-05-18 13:50:04 +0300

    Merge branch 'NF_v20' into 4ex7_CRM_v20
    # Conflicts:
    #	MultiRunner.py
    #	src/main/python/utils/config/Config.py

--- src/main/python/ui/crm/model/pages/financial_transactions/FinancialTransactionsPage.py
+++ src/main/python/ui/crm/model/pages/financial_transactions/FinancialTransactionsPage.py
@@ -223,22 +223,44 @@ class FinancialTransactionsPage(CRMBasePage):
         Logging().reportDebugStep(self, "Search form was opened")
         return FinancialTransactionsPage(self.driver)
 
+    def open_advanced_search(self):
+        sleep(0.1)
+        Logging().reportDebugStep(self, "Open an Advanced Search")
+        advanced_search = super().wait_load_element("//a[text()='Go to Advanced Search']")
+        self.driver.execute_script("arguments[0].click();", advanced_search)
+        return FinancialTransactionsPage(self.driver)
+
     def __fill_search_field_with_value(self, search_value):
         search_input = self.driver.find_element(By.XPATH, "//input[@name='search_text']")
         search_input.clear()
         search_input.send_keys(search_value)
 
+    def __fill_search_field_in_advanced_search(self, search_value):
+        sleep(0.1)
+        search_input = super().wait_load_element("//input[@id='fval0']")
+        search_input.clear()
+        search_input.send_keys(search_value)
+
+    def __select_search_list_in_advanced_search(self, search_value):
+        select = Select(self.driver.find_element(By.XPATH, "//select[@id='fval0']"))
+        select.select_by_visible_text(search_value)
+
     def __click_search_now_button(self):
-        search_now_button = self.driver.find_element(By.XPATH, "//input[@value=' Search Now ']")
+        search_now_button = self.driver.find_element(By.XPATH, "//input[@id='searchNow']")
         search_now_button.click()
 
     def __change_search_criteria_by_visible_text(self, criteria_from_drop_down_list):
-        select = Select(self.driver.find_element(By.XPATH, "//select[@name='search_field']"))
+        select = Select(self.driver.find_element(By.XPATH, "//select[@id='fcol0']"))
+        select.select_by_visible_text(criteria_from_drop_down_list)
+
+    def __select_search_condition_by_visible_text(self, criteria_from_drop_down_list):
+        select = Select(self.driver.find_element(By.XPATH, "//select[@id='fop0']"))
         select.select_by_visible_text(criteria_from_drop_down_list)
 
     def search_for_transaction_id(self, transaction_id):
         self.__change_search_criteria_by_visible_text("Transaction No")
-        self.__fill_search_field_with_value(transaction_id)
+        self.__select_search_condition_by_visible_text("equals")
+        self.__fill_search_field_in_advanced_search(transaction_id)
         self.__click_search_now_button()
         sleep(2)
         self.wait_vtiger_loading_to_finish_custom(35)
@@ -247,7 +269,8 @@ class FinancialTransactionsPage(CRMBasePage):
 
     def search_for_client_name(self, client_name):
         self.__change_search_criteria_by_visible_text("Client")
-        self.__fill_search_field_with_value(client_name)
+        self.__select_search_condition_by_visible_text("equals")
+        self.__fill_search_field_in_advanced_search(client_name)
         self.__click_search_now_button()
         sleep(1)
         self.wait_vtiger_loading_to_finish_custom(35)
@@ -256,7 +279,8 @@ class FinancialTransactionsPage(CRMBasePage):
 
     def search_for_transaction_type(self, transaction_type):
         self.__change_search_criteria_by_visible_text("Transaction Type")
-        self.__fill_search_field_with_value(transaction_type)
+        self.__select_search_condition_by_visible_text("equals")
+        self.__select_search_list_in_advanced_search(transaction_type)
         self.__click_search_now_button()
         sleep(1)
         self.wait_vtiger_loading_to_finish_custom(35)
@@ -265,7 +289,8 @@ class FinancialTransactionsPage(CRMBasePage):
 
     def search_for_modified_time(self, modified_time):
         self.__change_search_criteria_by_visible_text("Created Time")
-        self.__fill_search_field_with_value(modified_time)
+        self.__select_search_condition_by_visible_text("equals")
+        self.__fill_search_field_in_advanced_search(modified_time)
         self.__click_search_now_button()
         sleep(1)
         self.wait_vtiger_loading_to_finish_custom(35)
