c783401c9d7a9f308c26d9eb6fe963d95cb9fa33
michael_oryshchenko <michael_oryshchenko@pandats.com>
2020-05-28 13:56:53 +0300

    debug

--- src/main/python/ui/crm/model/pages/campaign_module_ui/CampaignCreatePage.py
+++ src/main/python/ui/crm/model/pages/campaign_module_ui/CampaignCreatePage.py
@@ -1,20 +1,15 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
-
-from src.main.python.ui.crm.model.pages.crm_base_page.CRMBasePage import CRMBasePage
-from src.main.python.utils.logs.Loging import Logging
 from time import sleep
-from src.main.python.ui.crm.model.pages.campaigns.CampaignsPage import CampaignsPage
-from selenium.webdriver import ActionChains
-from src.main.python.ui.crm.model.modules.campaigns_module.EditCampaignModule import EditCampaignModule
+# from src.main.python.ui.crm.model.pages.campaigns.CampaignsPage import CampaignsPage
 from src.main.python.ui.crm.model.pages.crm_base_page.CRMBasePage import CRMBasePage
 from src.main.python.utils.logs.Loging import Logging
 
 
-class AddCampaignsModule(CRMBasePage):
+class CampaignCreatePage(CRMBasePage):
 
-    def perform_add_new_campaign(self, name=None, assigned_to=None, deal=None, rate=None):
+    def add_campaign(self, name=None, assigned_to=None, deal=None, rate=None):
         if name:
             self.set_name(name)
         if assigned_to:
@@ -30,7 +25,7 @@ class AddCampaignsModule(CRMBasePage):
         search_button = super().wait_element_to_be_clickable("//input[@name='campaign_name']")
         search_button.send_keys(name)
         Logging().reportDebugStep(self, "The campaign name was set : "+name)
-        return AddCampaignsModule()
+        return CampaignCreatePage()
 
     def set_assigned_to(self, assigned_to):
         assigned_to_drop_down = super().wait_element_to_be_clickable("//span[@dir='ltr']")
@@ -40,7 +35,7 @@ class AddCampaignsModule(CRMBasePage):
         element = super().wait_element_to_be_clickable("//li[contains(text(),'%s')]" % assigned_to)
         element.click()
         Logging().reportDebugStep(self, "The assigned to was set: " + assigned_to)
-        return AddCampaignsModule()
+        return CampaignCreatePage()
 
     def set_start_date(self, start_date):
         start_date_button = super().wait_element_to_be_clickable("//input[@name='start_date']")
@@ -54,7 +49,7 @@ class AddCampaignsModule(CRMBasePage):
         sleep(1)
         # start_date_button.click()
         Logging().reportDebugStep(self, "The start date was set: " + start_date)
-        return AddCampaignsModule()
+        return CampaignCreatePage()
 
     def set_end_date(self, end_date):
         search_button = super().wait_element_to_be_clickable("//input[@name='end_date']")
@@ -67,14 +62,14 @@ class AddCampaignsModule(CRMBasePage):
         search_button.send_keys(Keys.ENTER)
         # search_button.click()
         Logging().reportDebugStep(self, "The end date was set: " + end_date)
-        return AddCampaignsModule()
+        return CampaignCreatePage()
 
     def set_rate(self, rate):
         rate_field = super().wait_element_to_be_clickable("//input[@name='deal_value']")
         rate_field.clear()
         rate_field.send_keys(rate)
         Logging().reportDebugStep(self, "The rate was set: " + rate)
-        return AddCampaignsModule()
+        return CampaignCreatePage()
 
     def click_save_button(self):
         save_button = super().wait_element_to_be_clickable("//button[@id='Save']")
@@ -82,7 +77,7 @@ class AddCampaignsModule(CRMBasePage):
         self.driver.execute_script("arguments[0].click();", save_button)
         # save_button.click()
         Logging().reportDebugStep(self, "The save button was clicked ")
-        return AddCampaignsModule()
+        return CampaignCreatePage()
 
     def set_deal(self, deal):
         deal_button = Select(self.driver.find_element(By.XPATH, "//select[@name='deal']"))
@@ -91,12 +86,11 @@ class AddCampaignsModule(CRMBasePage):
         # deal_drop_down.click()
         self.driver.execute_script("arguments[0].click();", deal_drop_down)
         Logging().reportDebugStep(self, "The deal was set ")
-        return AddCampaignsModule()
+        return CampaignCreatePage()
 
     def set_active_check_box(self):
         activity_button = super().wait_element_to_be_clickable("//input[@type='checkbox']")
         sleep(2)
         activity_button.click()
         Logging().reportDebugStep(self, "The active check box was set ")
-        return AddCampaignsModule()
-
+        return CampaignCreatePage()
