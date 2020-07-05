93c3f1509be60f00427daf95d223cced9a7eb33e
michael_oryshchenko <michael_oryshchenko@pandats.com>
2020-07-05 13:06:54 +0300

    debug

--- src/main/python/ui/crm/model/pages/trading_ui/TradingDetailsPageUI.py
+++ src/main/python/ui/crm/model/pages/trading_ui/TradingDetailsPageUI.py
@@ -14,25 +14,29 @@ class TradingDetailsPageUI(CRMBasePage):
 
     def display_open_trades(self):
         sleep(0.1)
-        btn = super().wait_element_to_be_clickable("//span[text()=' Display open trades ']")
-        self.driver.execute_script("arguments[0].click();", btn)
-        sleep(1)
-        self.wait_loading_to_finish_new_ui(25)
-        Logging().reportDebugStep(self, "Click 'Display open trades' button")
+        try:
+            btn = super().wait_element_to_be_clickable(
+                "//mat-expansion-panel-header[@aria-expanded='false']//div[text()=' Open Trades ']")
+            self.driver.execute_script("arguments[0].click();", btn)
+            sleep(1)
+            self.wait_loading_to_finish_new_ui(25)
+            Logging().reportDebugStep(self, "Click 'Open Trades' tab")
+        except:
+            Logging().reportDebugStep(self, "Tab 'Open Trades' already opened")
         return TradingDetailsPageUI(self.driver)
 
     def get_open_orders_data(self):
         sleep(0.2)
         open_orders_data = super().wait_load_element(
-            "(//div[div='Open trades']//following-sibling::pnd-grid//tbody/tr[not(contains(@style,'hidden'))])[1]")\
+            "//mat-expansion-panel[@id='open-trades']//tbody/tr[not (contains(@style,'hidden'))][1]")\
             .get_attribute("innerText")
         Logging().reportDebugStep(self, "Get Open Orders data: " + open_orders_data)
         return open_orders_data
 
-    def get_closed_orders_data_ui(self):
+    def get_closed_orders_data_ui(self, row):
         sleep(0.2)
         closed_orders_data = super().wait_load_element(
-            "//mat-expansion-panel[@id='closed-transactions']//tbody[@role='rowgroup']")\
+            "//mat-expansion-panel[@id='closed-trades']//tbody/tr[not (contains(@style,'hidden'))][%s]" % row)\
             .get_attribute("innerText")
         Logging().reportDebugStep(self, "Get Closed Orders data: " + closed_orders_data)
         return closed_orders_data
